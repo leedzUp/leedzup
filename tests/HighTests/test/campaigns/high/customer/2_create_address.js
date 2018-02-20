hatem-hadrich/PrestaShop

Create cutomer and address

tests/HighTests/test/campaigns/high/customer/2_create_address.js
@@ -1,0 +1,23 @@
+scenario('Create Address', client => {
+  test('should open the browser', () => client.open());
+  test('should sign in BO', () => client.signinBO());
+  test('should go to Address menu', () => client.goToCustomersAddress());
+  test('should click on add new address', () => client.newAddress());
+  test('should add customer email', () => client.addCustomerEmail());
+  test('should add identification number', () => client.addIDFiscale());
+  test('should add address alias', () => client.addAliasAddress());
+  test('should add first name', () => client.addFirstName());
+  test('should add last name', () => client.addLastName());
+  test('should add company', () => client.addCompanyName());
+  test('should add VAT number', () => client.addTVANumber());
+  test('should add address', () => client.addAddress());
+  test('should add second address', () => client.addSecondAddress());
+  test('should add postal code', () => client.addZIPCode());
+  test('should add city', () => client.addCity());
+  test('should add pays', () => client.addCountry());
+  test('should add home phone', () => client.addHomePhone());
+  test('should add other information', () => client.addOther());
+  test('should save the new address', () => client.saveAddress());
+  test('should check added customer success panel', () => client.successPanel('Successful creation.'));
+  test('should sign out BO', () => client.signoutBO());
+}, 'customer', true);

tests/HighTests/test/clients/customer.js
@@ -1,0 +1,194 @@
+var PrestashopClient = require('./prestashop_client');
+var {selector} = require('../globals.webdriverio.js');
+
+class CreateCustomer extends PrestashopClient {
+
+  goToCustomers() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.customer_menu, 90000)
+      .click(selector.BO.Customers.Customer.customer_menu)
+      .pause(2000)
+  }
+
+  addNewCustomer() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.new_customer_button, 90000)
+      .click(selector.BO.Customers.Customer.new_customer_button)
+      .pause(2000)
+  }
+
+  addSocialTitle() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.social_title_button, 90000)
+      .click(selector.BO.Customers.Customer.social_title_button)
+      .pause(2000)
+  }
+
+  addCustomerFirstName() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.first_name_input, 90000)
+      .setValue(selector.BO.Customers.Customer.first_name_input, 'Marion')
+      .pause(2000)
+  }
+
+  addCustomerLastName() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.last_name_input, 90000)
+      .setValue(selector.BO.Customers.Customer.last_name_input, 'Francois')
+      .pause(2000)
+  }
+
+  addEmailAddress() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.email_address_input, 90000)
+      .setValue(selector.BO.Customers.Customer.email_address_input, 'demo' + global.customer_id + '@prestashop.com')
+      .pause(1000)
+  }
+
+  addPassword() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.password_input, 90000)
+      .setValue(selector.BO.Customers.Customer.password_input, '123456789')
+      .pause(2000)
+  }
+
+  addBirthday() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.days_select, 90000)
+      .selectByValue(selector.BO.Customers.Customer.days_select, '18')
+      .waitForExist(selector.BO.Customers.Customer.month_select, 90000)
+      .selectByValue(selector.BO.Customers.Customer.month_select, '12')
+      .waitForExist(selector.BO.Customers.Customer.years_select, 90000)
+      .selectByValue(selector.BO.Customers.Customer.years_select, '1991')
+  }
+
+  saveCustomer() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.save_button, 90000)
+      .click(selector.BO.Customers.Customer.save_button)
+      .pause(1000)
+  }
+
+  goToCustomersAddress() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.customer_menu, 90000)
+      .moveToObject(selector.BO.Customers.Customer.customer_menu)
+      .waitForExist(selector.BO.Customers.addresses.addresses_menu, 90000)
+      .click(selector.BO.Customers.addresses.addresses_menu)
+      .pause(2000)
+  }
+
+  newAddress() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.new_address_button, 90000)
+      .click(selector.BO.Customers.addresses.new_address_button)
+      .pause(2000)
+  }
+
+  addCustomerEmail() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.email_input, 90000)
+      .setValue(selector.BO.Customers.addresses.email_input, 'demo' + global.customer_id + '@prestashop.com')
+      .pause(1000)
+  }
+
+  addIDFiscale() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.id_number_input, 90000)
+      .setValue(selector.BO.Customers.addresses.id_number_input, '0123456789')
+      .pause(1000)
+  }
+
+  addAliasAddress() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.address_alias_input, 90000)
+      .setValue(selector.BO.Customers.addresses.address_alias_input, 'Ma super addresse')
+      .pause(1000)
+  }
+
+  addFirstName() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.first_name_input, 90000)
+      .setValue(selector.BO.Customers.addresses.first_name_input, 'demo')
+      .pause(1000)
+  }
+
+  addLastName() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.last_name_input, 90000)
+      .setValue(selector.BO.Customers.addresses.last_name_input, 'demo')
+      .pause(1000)
+  }
+
+  addCompanyName() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.company, 90000)
+      .setValue(selector.BO.Customers.addresses.company, 'Presta')
+      .pause(1000)
+  }
+
+  addTVANumber() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.VAT_number_input, 90000)
+      .setValue(selector.BO.Customers.addresses.VAT_number_input, '0123456789')
+      .pause(1000)
+  }
+
+  addAddress() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.address_input, 90000)
+      .setValue(selector.BO.Customers.addresses.address_input, "12 rue d'amsterdam")
+      .pause(1000)
+  }
+
+  addSecondAddress() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.address_second_input, 90000)
+      .setValue(selector.BO.Customers.addresses.address_second_input, "RDC")
+      .pause(1000)
+  }
+
+  addZIPCode() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.zip_code_input, 90000)
+      .setValue(selector.BO.Customers.addresses.zip_code_input, "75009")
+      .pause(1000)
+  }
+
+  addCity() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.city_input, 90000)
+      .setValue(selector.BO.Customers.addresses.city_input, "Paris")
+      .pause(2000)
+  }
+
+  addCountry() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.country_input, 90000)
+      .selectByValue(selector.BO.Customers.addresses.country_input, "8")
+      .pause(1000)
+  }
+
+  addHomePhone() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.phone_input, 90000)
+      .setValue(selector.BO.Customers.addresses.phone_input, "0123456789")
+      .pause(1000)
+  }
+
+  addOther() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.other_input, 90000)
+      .setValue(selector.BO.Customers.addresses.other_input, "azerty")
+      .pause(1000)
+  }
+
+  saveAddress() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.save_button, 90000)
+      .click(selector.BO.Customers.addresses.save_button)
+      .pause(1000)
+  }
+}
+
+module.exports = CreateCustomer;

leedzUp/leedzup

tests/HighTests/test/campaigns/high/customer/2_create_address.js
@@ -1,0 +1,23 @@
+scenario('Create Address', client => {
+  test('should open the browser', () => client.open());
+  test('should sign in BO', () => client.signinBO());
+  test('should go to Address menu', () => client.goToCustomersAddress());
+  test('should click on add new address', () => client.newAddress());
+  test('should add customer email', () => client.addCustomerEmail());
+  test('should add identification number', () => client.addIdNumber());
+  test('should add address alias', () => client.addAliasAddress());
+  test('should add first name', () => client.addFirstName());
+  test('should add last name', () => client.addLastName());
+  test('should add company', () => client.addCompanyName());
+  test('should add VAT number', () => client.addTVANumber());
+  test('should add address', () => client.addAddress());
+  test('should add second address', () => client.addSecondAddress());
+  test('should add postal code', () => client.addZIPCode());
+  test('should add city', () => client.addCity());
+  test('should add pays', () => client.addCountry());
+  test('should add home phone', () => client.addHomePhone());
+  test('should add other information', () => client.addOther());
+  test('should save the new address', () => client.saveAddress());
+  test('should check that the success alert message is well displayed', () => client.successPanel('Successful creation.'));
+  test('should sign out BO', () => client.signoutBO());
+}, 'customer', true);

tests/HighTests/test/clients/customer.js
@@ -1,0 +1,194 @@
+var PrestashopClient = require('./prestashop_client');
+var {selector} = require('../globals.webdriverio.js');
+
+class CreateCustomer extends PrestashopClient {
+
+  goToCustomersMenu() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.customer_menu, 90000)
+      .click(selector.BO.Customers.Customer.customer_menu)
+      .pause(2000)
+  }
+
+  addNewCustomer() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.new_customer_button, 90000)
+      .click(selector.BO.Customers.Customer.new_customer_button)
+      .pause(2000)
+  }
+
+  addSocialTitle() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.social_title_button, 90000)
+      .click(selector.BO.Customers.Customer.social_title_button)
+      .pause(2000)
+  }
+
+  addCustomerFirstName() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.first_name_input, 90000)
+      .setValue(selector.BO.Customers.Customer.first_name_input, 'Marion')
+      .pause(2000)
+  }
+
+  addCustomerLastName() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.last_name_input, 90000)
+      .setValue(selector.BO.Customers.Customer.last_name_input, 'Francois')
+      .pause(2000)
+  }
+
+  addEmailAddress() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.email_address_input, 90000)
+      .setValue(selector.BO.Customers.Customer.email_address_input, 'demo' + global.product_id + '@prestashop.com')
+      .pause(1000)
+  }
+
+  addPassword() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.password_input, 90000)
+      .setValue(selector.BO.Customers.Customer.password_input, '123456789')
+      .pause(2000)
+  }
+
+  addBirthday() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.days_select, 90000)
+      .selectByValue(selector.BO.Customers.Customer.days_select, '18')
+      .waitForExist(selector.BO.Customers.Customer.month_select, 90000)
+      .selectByValue(selector.BO.Customers.Customer.month_select, '12')
+      .waitForExist(selector.BO.Customers.Customer.years_select, 90000)
+      .selectByValue(selector.BO.Customers.Customer.years_select, '1991')
+  }
+
+  saveCustomer() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.save_button, 90000)
+      .click(selector.BO.Customers.Customer.save_button)
+      .pause(1000)
+  }
+
+  goToCustomersAddress() {
+    return this.client
+      .waitForExist(selector.BO.Customers.Customer.customer_menu, 90000)
+      .moveToObject(selector.BO.Customers.Customer.customer_menu)
+      .waitForExist(selector.BO.Customers.addresses.addresses_menu, 90000)
+      .click(selector.BO.Customers.addresses.addresses_menu)
+      .pause(2000)
+  }
+
+  newAddress() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.new_address_button, 90000)
+      .click(selector.BO.Customers.addresses.new_address_button)
+      .pause(2000)
+  }
+
+  addCustomerEmail() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.email_input, 90000)
+      .setValue(selector.BO.Customers.addresses.email_input, 'demo' + global.product_id + '@prestashop.com')
+      .pause(1000)
+  }
+
+  addIdNumber() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.id_number_input, 90000)
+      .setValue(selector.BO.Customers.addresses.id_number_input, '0123456789')
+      .pause(1000)
+  }
+
+  addAliasAddress() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.address_alias_input, 90000)
+      .setValue(selector.BO.Customers.addresses.address_alias_input, 'Ma super addresse')
+      .pause(1000)
+  }
+
+  addFirstName() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.first_name_input, 90000)
+      .setValue(selector.BO.Customers.addresses.first_name_input, 'demo')
+      .pause(1000)
+  }
+
+  addLastName() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.last_name_input, 90000)
+      .setValue(selector.BO.Customers.addresses.last_name_input, 'demo')
+      .pause(1000)
+  }
+
+  addCompanyName() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.company, 90000)
+      .setValue(selector.BO.Customers.addresses.company, 'Presta')
+      .pause(1000)
+  }
+
+  addTVANumber() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.VAT_number_input, 90000)
+      .setValue(selector.BO.Customers.addresses.VAT_number_input, '0123456789')
+      .pause(1000)
+  }
+
+  addAddress() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.address_input, 90000)
+      .setValue(selector.BO.Customers.addresses.address_input, "12 rue d'amsterdam")
+      .pause(1000)
+  }
+
+  addSecondAddress() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.address_second_input, 90000)
+      .setValue(selector.BO.Customers.addresses.address_second_input, "RDC")
+      .pause(1000)
+  }
+
+  addZIPCode() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.zip_code_input, 90000)
+      .setValue(selector.BO.Customers.addresses.zip_code_input, "75009")
+      .pause(1000)
+  }
+
+  addCity() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.city_input, 90000)
+      .setValue(selector.BO.Customers.addresses.city_input, "Paris")
+      .pause(2000)
+  }
+
+  addCountry() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.country_input, 90000)
+      .selectByValue(selector.BO.Customers.addresses.country_input, "8")
+      .pause(1000)
+  }
+
+  addHomePhone() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.phone_input, 90000)
+      .setValue(selector.BO.Customers.addresses.phone_input, "0123456789")
+      .pause(1000)
+  }
+
+  addOther() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.other_input, 90000)
+      .setValue(selector.BO.Customers.addresses.other_input, "azerty")
+      .pause(1000)
+  }
+
+  saveAddress() {
+    return this.client
+      .waitForExist(selector.BO.Customers.addresses.save_button, 90000)
+      .click(selector.BO.Customers.addresses.save_button)
+      .pause(1000)
+  }
+}
+
+module.exports = CreateCustomer;