PrestaShop/PrestaShop

add new optional head_seo_title in CMS pages

controllers/admin/AdminCmsController.php
@@ -59,7 +59,8 @@ class AdminCmsControllerCore extends AdminController
             'id_cms' => array('title' => $this->trans('ID', array(), 'Admin.Global'), 'align' => 'center', 'class' => 'fixed-width-xs'),
             'link_rewrite' => array('title' => $this->trans('URL', array(), 'Admin.Global')),
             'meta_title' => array('title' => $this->trans('Title', array(), 'Admin.Global'), 'filter_key' => 'b!meta_title'),
-            'position' => array('title' => $this->trans('Position', array(), 'Admin.Global'), 'filter_key' => 'position', 'align' => 'center', 'class' => 'fixed-width-sm', 'position' => 'position'),
+            'head_seo_title' => array('title' => $this->trans('Head title', array(), 'Admin.Global'), 'filter_key' => 'b!head_seo_title'),
+            'position' => array('title' => $this->trans('Position', array(), 'Admin.Global'),'filter_key' => 'position', 'align' => 'center', 'class' => 'fixed-width-sm', 'position' => 'position'),
             'active' => array('title' => $this->trans('Displayed', array(), 'Admin.Global'), 'align' => 'center', 'active' => 'status', 'class' => 'fixed-width-sm', 'type' => 'bool', 'orderby' => false),
         );
 
@@ -142,6 +143,16 @@ class AdminCmsControllerCore extends AdminController
                     'required' => true,
                     'class' => 'copyMeta2friendlyURL',
                     'hint' => $this->trans('Invalid characters:', array(), 'Admin.Notifications.Info') . ' &lt;&gt;;=#{}',
+                ),
+                array(
+                    'type' => 'text',
+                    'label' => $this->trans('Head title', array(), 'Admin.Global'),
+                    'name' => 'head_seo_title',
+                    'lang' => true,
+                    'hint' => array(
+                        $this->trans('Used to override the title tag value. If left blank the default title value is used.', array(), 'Admin.Design.Help'),
+                        $this->trans('Invalid characters:', array(), 'Admin.Notifications.Info') . ' &lt;&gt;;=#{}',
+                    ),
                 ),
                 array(
                     'type' => 'text',

leedzUp/leedzup

controllers/admin/AdminCmsController.php
@@ -59,6 +59,7 @@ class AdminCmsControllerCore extends AdminController
             'id_cms' => array('title' => $this->trans('ID', array(), 'Admin.Global'), 'align' => 'center', 'class' => 'fixed-width-xs'),
             'link_rewrite' => array('title' => $this->trans('URL', array(), 'Admin.Global')),
             'meta_title' => array('title' => $this->trans('Title', array(), 'Admin.Global'), 'filter_key' => 'b!meta_title'),
+            'head_seo_title' => array('title' => $this->trans('Meta title', array(), 'Admin.Global'), 'filter_key' => 'b!head_seo_title'),
             'position' => array('title' => $this->trans('Position', array(), 'Admin.Global'), 'filter_key' => 'position', 'align' => 'center', 'class' => 'fixed-width-sm', 'position' => 'position'),
             'active' => array('title' => $this->trans('Displayed', array(), 'Admin.Global'), 'align' => 'center', 'active' => 'status', 'class' => 'fixed-width-sm', 'type' => 'bool', 'orderby' => false),
         );
@@ -135,13 +136,26 @@ class AdminCmsControllerCore extends AdminController
                 ),
                 array(
                     'type' => 'text',
-                    'label' => $this->trans('Meta title', array(), 'Admin.Global'),
+                    'label' => $this->trans('Title', array(), 'Admin.Global'),
                     'name' => 'meta_title',
                     'id' => 'name', // for copyMeta2friendlyURL compatibility
                     'lang' => true,
                     'required' => true,
                     'class' => 'copyMeta2friendlyURL',
-                    'hint' => $this->trans('Invalid characters:', array(), 'Admin.Notifications.Info') . ' &lt;&gt;;=#{}',
+                    'hint' => array(
+                        $this->trans('Used in the h1 page tag, and as the default title tag value.', array(), 'Admin.Design.Help'),
+                        $this->trans('Invalid characters:', array(), 'Admin.Notifications.Info') . ' &lt;&gt;;=#{}',
+                    ),
+                ),
+                array(
+                    'type' => 'text',
+                    'label' => $this->trans('Meta title', array(), 'Admin.Global'),
+                    'name' => 'head_seo_title',
+                    'lang' => true,
+                    'hint' => array(
+                        $this->trans('Used to override the title tag value. If left blank the default title value is used.', array(), 'Admin.Design.Help'),
+                        $this->trans('Invalid characters:', array(), 'Admin.Notifications.Info') . ' &lt;&gt;;=#{}',
+                    ),
                 ),
                 array(
                     'type' => 'text',