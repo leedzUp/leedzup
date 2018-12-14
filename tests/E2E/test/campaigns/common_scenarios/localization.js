PrestaShop/PrestaShop

TE: import localization

tests/E2E/test/campaigns/common_scenarios/localization.js
@@ -7,8 +7,68 @@ const {Menu} = require('../../selectors/BO/menu.js');
 const {ThemeAndLogo} = require('../../selectors/BO/design/theme_and_logo');
 const Design = require('../../selectors/BO/design/index');
 const {languageFO} = require('../../selectors/FO/index');
+let promise = Promise.resolve();
 
 module.exports = {
+  importLocalization(language, localization, downloadPackData = false, contentImport = false) {
+    scenario('Import a localization pack', client => {
+      test('should go to "Localization" page', () => client.goToSubtabMenuPage(Menu.Improve.International.international_menu, Menu.Improve.International.localization_submenu));
+      test('should click on "Localization pack you want to import" select', () => client.waitForExistAndClick(Localization.Localization.pack_select));
+      test('should search for the localization from the select', () => client.waitAndSetValue(Localization.Localization.pack_search_input, language));
+      test('should click on "' + localization.toUpperCase() + '" option', () => client.waitForExistAndClick(Localization.Localization.pack_option.replace('%B', localization)));
+      if (contentImport === true) {
+        test('should uncheck "States"', () => client.waitForExistAndClick(Localization.Localization.content_import_checkbox.replace('%B', 0)));
+        test('should uncheck "Taxes"', () => client.waitForExistAndClick(Localization.Localization.content_import_checkbox.replace('%B', 1)));
+        test('should uncheck "Units"', () => client.waitForExistAndClick(Localization.Localization.content_import_checkbox.replace('%B', 4)));
+      }
+      if (downloadPackData === true) {
+        test('should put "Download pack data" toggle button on "No"', () => client.waitForExistAndClick(Localization.Localization.download_pack_data_toggle_button));
+      }
+      test('should click on "Import" button', () => client.waitForExistAndClick(Localization.Localization.import_button));
+      test('should verify the appearance of the green validation', () => client.checkTextValue(Localization.Localization.success_alert_panel.replace('%B', 'alert-success'), 'Localization pack imported successfully.'));
+    }, 'common_client');
+  },
+  getDefaultConfiguration() {
+    scenario('Get the default configuration values', client => {
+      test('should go to "Localization" page', () => client.goToSubtabMenuPage(Menu.Improve.International.international_menu, Menu.Improve.International.localization_submenu));
+      test('should get the default language', () => client.getAttributeInVar(Localization.Localization.configuration_selected_option.replace('%ID', 'form_configuration_default_language'), 'value', 'firstDefaultLanguageValue'));
+      test('should get the default country', () => client.getAttributeInVar(Localization.Localization.configuration_selected_option.replace('%ID', 'form_configuration_default_country'), 'value', 'firstDefaultCountryValue'));
+      test('should get the default currency', () => client.getAttributeInVar(Localization.Localization.configuration_selected_option.replace('%ID', 'form_configuration_default_currency'), 'value', 'firstDefaultCurrencyValue'));
+      test('should get the time zone', () => client.getAttributeInVar(Localization.Localization.configuration_selected_option.replace('%ID', 'form_configuration_timezone'), 'value', 'firstDefaultTimezoneValue'));
+    }, 'common_client');
+  },
+  checkUnchangedDefaultConfiguration() {
+    scenario('Check the unchanged default configuration', client => {
+      test('should Verify if the default language has not changed', () => {
+        return promise
+          .then(() => client.getAttributeInVar(Localization.Localization.configuration_selected_option.replace('%ID', 'form_configuration_default_language'), 'value', 'secondDefaultLanguageValue'))
+          .then(() => expect(tab['secondDefaultLanguageValue']).to.be.equal(tab['firstDefaultLanguageValue']));
+      });
+      test('should Verify if the default country has not changed', () => {
+        return promise
+          .then(() => client.getAttributeInVar(Localization.Localization.configuration_selected_option.replace('%ID', 'form_configuration_default_country'), 'value', 'secondDefaultCountryValue'))
+          .then(() => expect(tab['secondDefaultCountryValue']).to.be.equal(tab['firstDefaultCountryValue']));
+      });
+      test('should Verify if the default currency has not changed', () => {
+        return promise
+          .then(() => client.getAttributeInVar(Localization.Localization.configuration_selected_option.replace('%ID', 'form_configuration_default_currency'), 'value', 'secondDefaultCurrencyValue'))
+          .then(() => expect(tab['secondDefaultCurrencyValue']).to.be.equal(tab['firstDefaultCurrencyValue']));
+      });
+      test('should Verify if the time zone has not changed', () => {
+        return promise
+          .then(() => client.getAttributeInVar(Localization.Localization.configuration_selected_option.replace('%ID', 'form_configuration_timezone'), 'value', 'secondDefaultTimezoneValue'))
+          .then(() => expect(tab['secondDefaultTimezoneValue']).to.be.equal(tab['firstDefaultTimezoneValue']));
+      });
+    }, 'common_client');
+  },
+  checkExistenceLanguage(language) {
+    scenario('Check the existence language in the Back Office', client => {
+      test('should click on "Languages" subtab', () => client.waitForExistAndClick(Menu.Improve.International.languages_tab));
+      test('should search for "' + language.toUpperCase() + '" language', () => client.searchByValue(Localization.Localization.language_name_input, Localization.Localization.search_button, language));
+      test('should check  "' + language.toUpperCase() + '" language exists', () => client.checkTextValue(Localization.Localization.language_column.replace("%ID", 4), language, 'contain'));
+      test('should click on "Reset" button', () => client.waitForExistAndClick(Localization.Localization.reset_button));
+    }, 'common_client');
+  },
   createLanguage: function (languageData) {
     scenario('Create a new "Language"', client => {
       test('should go to "Localization" page', () => client.goToSubtabMenuPage(Menu.Improve.International.international_menu, Menu.Improve.International.localization_submenu));
@@ -118,11 +178,15 @@ module.exports = {
       }
     }, 'common_client');
   },
-  deleteLanguage: function (name) {
+  deleteLanguage: function (name, dateTime = true) {
     scenario('Delete the created "Language"', client => {
       test('should go to "Localization" page', () => client.goToSubtabMenuPage(Menu.Improve.International.international_menu, Menu.Improve.International.localization_submenu));
       test('should click on "Languages" tab', () => client.waitForExistAndClick(Menu.Improve.International.languages_tab));
-      test('should search for the created language', () => client.searchByValue(Localization.languages.filter_name_input, Localization.languages.filter_search_button, name + date_time));
+      if (dateTime === false) {
+        test('should search for the created language', () => client.searchByValue(Localization.languages.filter_name_input, Localization.languages.filter_search_button, name));
+      } else {
+        test('should search for the created language', () => client.searchByValue(Localization.languages.filter_name_input, Localization.languages.filter_search_button, name + date_time));
+      }
       test('should click on "dropdown toggle" button', () => client.waitForExistAndClick(Localization.languages.dropdown_button));
       test('should click on "Delete" button', () => client.waitForExistAndClick(Localization.languages.delete_button));
       test('should accept the confirmation alert', () => client.alertAccept());

leedzUp/leedzup

tests/E2E/test/campaigns/common_scenarios/localization.js
@@ -18,6 +18,65 @@ let promise = Promise.resolve();
  */
 
 module.exports = {
+  importLocalization(language, localization, downloadPackData = false, contentImport = false) {
+    scenario('Import a localization pack', client => {
+      test('should go to "International > Localization" page', () => client.goToSubtabMenuPage(Menu.Improve.International.international_menu, Menu.Improve.International.localization_submenu));
+      test('should click on "Localization pack you want to import" select', () => client.waitForExistAndClick(Localization.Localization.pack_select));
+      test('should search for the localization from the select', () => client.waitAndSetValue(Localization.Localization.pack_search_input, language));
+      test('should click on "' + localization.toUpperCase() + '" option', () => client.waitForExistAndClick(Localization.Localization.pack_option.replace('%B', localization)));
+      if (contentImport === true) {
+        test('should uncheck "States"', () => client.waitForExistAndClick(Localization.Localization.content_import_checkbox.replace('%B', 0)));
+        test('should uncheck "Taxes"', () => client.waitForExistAndClick(Localization.Localization.content_import_checkbox.replace('%B', 1)));
+        test('should uncheck "Units"', () => client.waitForExistAndClick(Localization.Localization.content_import_checkbox.replace('%B', 4)));
+      }
+      if (downloadPackData === true) {
+        test('should put "Download pack data" toggle button on "No"', () => client.waitForExistAndClick(Localization.Localization.download_pack_data_toggle_button));
+      }
+      test('should click on "Import" button', () => client.waitForExistAndClick(Localization.Localization.import_button));
+      test('should verify the appearance of the green validation', () => client.checkTextValue(Localization.Localization.success_alert_panel.replace('%B', 'alert-success'), 'Localization pack imported successfully.'));
+    }, 'common_client');
+  },
+  getDefaultConfiguration() {
+    scenario('Get the default configuration values', client => {
+      test('should go to "Localization" page', () => client.goToSubtabMenuPage(Menu.Improve.International.international_menu, Menu.Improve.International.localization_submenu));
+      test('should get the default language', () => client.getAttributeInVar(Localization.Localization.configuration_selected_option.replace('%ID', 'form_configuration_default_language'), 'value', 'firstDefaultLanguageValue'));
+      test('should get the default country', () => client.getAttributeInVar(Localization.Localization.configuration_selected_option.replace('%ID', 'form_configuration_default_country'), 'value', 'firstDefaultCountryValue'));
+      test('should get the default currency', () => client.getAttributeInVar(Localization.Localization.configuration_selected_option.replace('%ID', 'form_configuration_default_currency'), 'value', 'firstDefaultCurrencyValue'));
+      test('should get the time zone', () => client.getAttributeInVar(Localization.Localization.configuration_selected_option.replace('%ID', 'form_configuration_timezone'), 'value', 'firstDefaultTimezoneValue'));
+    }, 'common_client');
+  },
+  checkUnchangedDefaultConfiguration() {
+    scenario('Check the unchanged default configuration', client => {
+      test('should Verify if the default language has not changed', () => {
+        return promise
+          .then(() => client.getAttributeInVar(Localization.Localization.configuration_selected_option.replace('%ID', 'form_configuration_default_language'), 'value', 'secondDefaultLanguageValue'))
+          .then(() => expect(tab['secondDefaultLanguageValue']).to.be.equal(tab['firstDefaultLanguageValue']));
+      });
+      test('should Verify if the default country has not changed', () => {
+        return promise
+          .then(() => client.getAttributeInVar(Localization.Localization.configuration_selected_option.replace('%ID', 'form_configuration_default_country'), 'value', 'secondDefaultCountryValue'))
+          .then(() => expect(tab['secondDefaultCountryValue']).to.be.equal(tab['firstDefaultCountryValue']));
+      });
+      test('should Verify if the default currency has not changed', () => {
+        return promise
+          .then(() => client.getAttributeInVar(Localization.Localization.configuration_selected_option.replace('%ID', 'form_configuration_default_currency'), 'value', 'secondDefaultCurrencyValue'))
+          .then(() => expect(tab['secondDefaultCurrencyValue']).to.be.equal(tab['firstDefaultCurrencyValue']));
+      });
+      test('should Verify if the time zone has not changed', () => {
+        return promise
+          .then(() => client.getAttributeInVar(Localization.Localization.configuration_selected_option.replace('%ID', 'form_configuration_timezone'), 'value', 'secondDefaultTimezoneValue'))
+          .then(() => expect(tab['secondDefaultTimezoneValue']).to.be.equal(tab['firstDefaultTimezoneValue']));
+      });
+    }, 'common_client');
+  },
+  checkExistenceLanguage(language) {
+    scenario('Check the existence language in the Back Office', client => {
+      test('should click on "Languages" subtab', () => client.waitForExistAndClick(Menu.Improve.International.languages_tab));
+      test('should search for "' + language.toUpperCase() + '" language', () => client.searchByValue(Localization.languages.filter_name_input, Localization.languages.filter_search_button, language));
+      test('should check  "' + language.toUpperCase() + '" language exists', () => client.checkTextValue(Localization.languages.language_column.replace("%ID", 4), language, 'contain'));
+      test('should click on "Reset" button', () => client.waitForExistAndClick(Localization.languages.reset_button));
+    }, 'common_client');
+  },
   createLanguage: function (languageData) {
     scenario('Create a new "Language"', client => {
       test('should go to "Localization" page', () => client.goToSubtabMenuPage(Menu.Improve.International.international_menu, Menu.Improve.International.localization_submenu));
@@ -127,11 +186,15 @@ module.exports = {
       }
     }, 'common_client');
   },
-  deleteLanguage: function (name) {
+  deleteLanguage: function (name, dateTime = true) {
     scenario('Delete the created "Language"', client => {
       test('should go to "Localization" page', () => client.goToSubtabMenuPage(Menu.Improve.International.international_menu, Menu.Improve.International.localization_submenu));
       test('should click on "Languages" tab', () => client.waitForExistAndClick(Menu.Improve.International.languages_tab));
-      test('should search for the created language', () => client.searchByValue(Localization.languages.filter_name_input, Localization.languages.filter_search_button, name + date_time));
+      if (dateTime === false) {
+        test('should search for the created language', () => client.searchByValue(Localization.languages.filter_name_input, Localization.languages.filter_search_button, name));
+      } else {
+        test('should search for the created language', () => client.searchByValue(Localization.languages.filter_name_input, Localization.languages.filter_search_button, name + date_time));
+      }
       test('should click on "dropdown toggle" button', () => client.waitForExistAndClick(Localization.languages.dropdown_button));
       test('should click on "Delete" button', () => client.waitForExistAndClick(Localization.languages.delete_button));
       test('should accept the confirmation alert', () => client.alertAccept());