PrestaShop/PrestaShop

Refactors reduction constraint

src/PrestaShopBundle/Controller/Admin/Sell/Catalog/CatalogPriceRuleController.php
@@ -26,11 +26,11 @@
 
 namespace PrestaShopBundle\Controller\Admin\Sell\Catalog;
 
-use PrestaShop\PrestaShop\Core\Domain\CatalogPriceRule\Exception\CatalogPriceRuleException;
 use PrestaShop\PrestaShop\Core\Domain\CatalogPriceRule\Exception\CatalogPriceRuleNotFoundException;
 use PrestaShop\PrestaShop\Core\Domain\CatalogPriceRule\Exception\UpdateCatalogPriceRuleException;
 use PrestaShop\PrestaShop\Core\Domain\CatalogPriceRule\Query\GetCatalogPriceRuleForEditing;
 use PrestaShop\PrestaShop\Core\Domain\CatalogPriceRule\QueryResult\EditableCatalogPriceRule;
+use PrestaShop\PrestaShop\Core\Domain\Exception\DomainConstraintException;
 use PrestaShop\PrestaShop\Core\Form\IdentifiableObject\Builder\FormBuilderInterface;
 use PrestaShop\PrestaShop\Core\Form\IdentifiableObject\Handler\FormHandlerInterface;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
@@ -65,7 +65,7 @@ class CatalogPriceRuleController extends FrameworkBundleAdminController
 
                 return $this->redirectToRoute('admin_catalog_price_rules_index');
             }
-        } catch (CatalogPriceRuleException $e) {
+        } catch (DomainConstraintException $e) {
             $this->addFlash('error', $this->getErrorMessageForException($e, $this->getErrorMessages()));
         }
 
@@ -101,7 +101,7 @@ class CatalogPriceRuleController extends FrameworkBundleAdminController
 
                 return $this->redirectToRoute('admin_catalog_price_rules_index');
             }
-        } catch (CatalogPriceRuleException $e) {
+        } catch (DomainConstraintException $e) {
             $this->addFlash('error', $this->getErrorMessageForException($e, $this->getErrorMessages()));
 
             if ($e instanceof CatalogPriceRuleNotFoundException) {

leedzUp/leedzup

src/PrestaShopBundle/Controller/Admin/Sell/Catalog/CatalogPriceRuleController.php
@@ -32,7 +32,6 @@ use PrestaShop\PrestaShop\Core\Domain\CatalogPriceRule\Command\DeleteCatalogPric
 use PrestaShop\PrestaShop\Core\Domain\CatalogPriceRule\Exception\DeleteCatalogPriceRuleException;
 use PrestaShop\PrestaShop\Core\Grid\Definition\Factory\CatalogPriceRuleGridDefinitionFactory;
 use PrestaShop\PrestaShop\Core\Search\Filters\CatalogPriceRuleFilters;
-use PrestaShop\PrestaShop\Core\Domain\CatalogPriceRule\Exception\CatalogPriceRuleException;
 use PrestaShop\PrestaShop\Core\Domain\CatalogPriceRule\Exception\CatalogPriceRuleNotFoundException;
 use PrestaShop\PrestaShop\Core\Domain\CatalogPriceRule\Exception\UpdateCatalogPriceRuleException;
 use PrestaShop\PrestaShop\Core\Domain\CatalogPriceRule\Query\GetCatalogPriceRuleForEditing;
@@ -208,7 +207,7 @@ class CatalogPriceRuleController extends FrameworkBundleAdminController
 
                 return $this->redirectToRoute('admin_catalog_price_rules_index');
             }
-        } catch (CatalogPriceRuleException $e) {
+        } catch (Exception $e) {
             $this->addFlash('error', $this->getErrorMessageForException($e, $this->getErrorMessages()));
 
             if ($e instanceof CatalogPriceRuleNotFoundException) {