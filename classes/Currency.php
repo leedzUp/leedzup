PrestaShop/PrestaShop

Improve behat quick currency creation for missing values, add transformation in Currency, improve middleware layers to manage currency pattern, Create PatternTransformer to transform symbol position in a currency pattern, Change php-csfixer config to avoid removing non-breaking space, NumberSpecicification::toArray automatically includes the numberSymbols

classes/Currency.php
@@ -153,6 +153,20 @@ class CurrencyCore extends ObjectModel
     public $precision;
 
     /**
+     * CLDR price formatting transformation
+     *
+     * @var string
+     */
+    public $transformation;
+
+    /**
+     * CLDR price formatting transform, the array is indexed by language id
+     *
+     * @var string[]
+     */
+    private $localizedTransformations;
+
+    /**
      * @see ObjectModel::$definition
      */
     public static $definition = array(
@@ -172,6 +186,7 @@ class CurrencyCore extends ObjectModel
             /* Lang fields */
             'name' => array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isGenericName', 'size' => 255),
             'symbol' => array('type' => self::TYPE_STRING, 'lang' => true, 'size' => 255),
+            'transformation' => array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isCurrencyPatternTransformation', 'size' => 64),
         ),
     );
 
@@ -235,8 +250,14 @@ class CurrencyCore extends ObjectModel
                 $this->localizedNames = $this->name;
                 $this->name = Tools::ucfirst($this->name[$idLang]);
             } else {
-                $this->localizedNames = [$idLang = $this->name];
+                $this->localizedNames = [$idLang => $this->name];
                 $this->name = Tools::ucfirst($this->name);
+            }
+
+            if (is_array($this->transformation)) {
+                $this->localizedTransformations = $this->transformation;
+            } else {
+                $this->localizedTransformations = [$idLang => $this->transformation];
             }
 
             $this->iso_code_num = $this->numeric_iso_code;
@@ -472,6 +493,37 @@ class CurrencyCore extends ObjectModel
         return $this;
     }
 
+    /**
+     * @return string
+     */
+    public function getPatternTransformation($idLang)
+    {
+        return !empty($this->localizedTransformations[$idLang]) ? $this->localizedTransformations[$idLang] : '';
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getLocalizedTransformations()
+    {
+        return $this->localizedTransformations;
+    }
+
+    /**
+     * This setter updates the transformation field because it is used when you want to update
+     * the database (legacy core feature). But to be consistent the transformations field also
+     * needs to be updated.
+     *
+     * @param string[] $localizedTransformations list of currency transformations, the array needs to be indexed by language id
+     *
+     * @return CurrencyCore
+     */
+    public function setLocalizedTransformations(array $localizedTransformations)
+    {
+        $this->localizedTransformations = $this->transformation = $localizedTransformations;
+
+        return $this;
+    }
     /**
      * Return available currencies.
      *
@@ -974,8 +1026,9 @@ class CurrencyCore extends ObjectModel
 
             if (empty($cldrCurrency)) {
                 // The currency may not be declared in the locale, eg with unofficial iso code
-                $namesByLang[$language->id] = $originalNames[$language->id] ?: $this->iso_code;
-                $symbolsByLang[$language->id] = $originalSymbols[$language->id] ?: $this->iso_code;
+                // Check if data is present in case it's not available for all languages
+                $namesByLang[$language->id] = !empty($originalNames[$language->id]) ? $originalNames[$language->id] : $this->iso_code;
+                $symbolsByLang[$language->id] = !empty($originalSymbols[$language->id]) ? $originalSymbols[$language->id] : $this->iso_code;
                 $this->modified = true;
                 continue;
             }
@@ -997,7 +1050,7 @@ class CurrencyCore extends ObjectModel
             $namesByLang[$language->id] = $name;
         }
 
-        $this->name = $namesByLang;
-        $this->symbol = $symbolsByLang;
+        $this->names = $this->name = $namesByLang;
+        $this->symbols = $this->symbol = $symbolsByLang;
     }
 }

src/Adapter/Currency/CommandHandler/EditUnofficialCurrencyHandler.php
@@ -127,6 +127,9 @@ final class EditUnofficialCurrencyHandler extends AbstractCurrencyHandler implem
         if (!empty($command->getLocalizedSymbols())) {
             $entity->setLocalizedSymbols($command->getLocalizedSymbols());
         }
+        if (!empty($command->getTransformations())) {
+            $entity->setLocalizedTransformations($command->getTransformations());
+        }
 
         $this->refreshLocalizedData($entity);
 

leedzUp/leedzup

classes/Currency.php
@@ -153,6 +153,20 @@ class CurrencyCore extends ObjectModel
     public $precision;
 
     /**
+     * CLDR price pattern
+     *
+     * @var string
+     */
+    public $pattern;
+
+    /**
+     * CLDR price patterns, the array is indexed by language id
+     *
+     * @var string[]
+     */
+    private $localizedPatterns;
+
+    /**
      * @see ObjectModel::$definition
      */
     public static $definition = array(
@@ -172,6 +186,7 @@ class CurrencyCore extends ObjectModel
             /* Lang fields */
             'name' => array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isGenericName', 'size' => 255),
             'symbol' => array('type' => self::TYPE_STRING, 'lang' => true, 'size' => 255),
+            'pattern' => array('type' => self::TYPE_STRING, 'lang' => true, 'size' => 64),
         ),
     );
 
@@ -235,8 +250,14 @@ class CurrencyCore extends ObjectModel
                 $this->localizedNames = $this->name;
                 $this->name = Tools::ucfirst($this->name[$idLang]);
             } else {
-                $this->localizedNames = [$idLang = $this->name];
+                $this->localizedNames = [$idLang => $this->name];
                 $this->name = Tools::ucfirst($this->name);
+            }
+
+            if (is_array($this->pattern)) {
+                $this->localizedPatterns = $this->pattern;
+            } else {
+                $this->localizedPatterns = [$idLang => $this->pattern];
             }
 
             $this->iso_code_num = $this->numeric_iso_code;
@@ -468,6 +489,38 @@ class CurrencyCore extends ObjectModel
     public function setLocalizedSymbols(array $localizedSymbols)
     {
         $this->localizedSymbols = $this->symbol = $localizedSymbols;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPattern($idLang)
+    {
+        return $this->localizedPatterns[$idLang] ?? '';
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getLocalizedPatterns()
+    {
+        return $this->localizedPatterns;
+    }
+
+    /**
+     * This setter updates the pattern field because it is used when you want to update
+     * the database (legacy core feature). But to be consistent the patterns field also
+     * needs to be updated.
+     *
+     * @param string[] $localizedPatterns list of currency patterns, the array needs to be indexed by language id
+     *
+     * @return CurrencyCore
+     */
+    public function setLocalizedPatterns(array $localizedPatterns)
+    {
+        $this->localizedPatterns = $this->pattern = $localizedPatterns;
 
         return $this;
     }
@@ -974,8 +1027,9 @@ class CurrencyCore extends ObjectModel
 
             if (empty($cldrCurrency)) {
                 // The currency may not be declared in the locale, eg with unofficial iso code
-                $namesByLang[$language->id] = $originalNames[$language->id] ?: $this->iso_code;
-                $symbolsByLang[$language->id] = $originalSymbols[$language->id] ?: $this->iso_code;
+                // Check if data is present in case it's not available for all languages
+                $namesByLang[$language->id] = !empty($originalNames[$language->id]) ? $originalNames[$language->id] : $this->iso_code;
+                $symbolsByLang[$language->id] = !empty($originalSymbols[$language->id]) ? $originalSymbols[$language->id] : $this->iso_code;
                 $this->modified = true;
                 continue;
             }
@@ -997,7 +1051,7 @@ class CurrencyCore extends ObjectModel
             $namesByLang[$language->id] = $name;
         }
 
-        $this->name = $namesByLang;
-        $this->symbol = $symbolsByLang;
+        $this->localizedNames = $this->name = $namesByLang;
+        $this->localizedSymbols = $this->symbol = $symbolsByLang;
     }
 }

src/Adapter/Currency/CommandHandler/EditUnofficialCurrencyHandler.php
@@ -127,6 +127,9 @@ final class EditUnofficialCurrencyHandler extends AbstractCurrencyHandler implem
         if (!empty($command->getLocalizedSymbols())) {
             $entity->setLocalizedSymbols($command->getLocalizedSymbols());
         }
+        if (!empty($command->getLocalizedPatterns())) {
+            $entity->setLocalizedPatterns($command->getLocalizedPatterns());
+        }
 
         $this->refreshLocalizedData($entity);
 