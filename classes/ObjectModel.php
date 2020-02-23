PrestaShop/PrestaShop

Fix & remove unneeded reference operators

classes/ObjectModel.php
@@ -661,10 +661,10 @@ abstract class ObjectModelCore implements \PrestaShop\PrestaShop\Core\Foundation
                 return false;
             }
 
-            foreach ($result as &$row) {
-                foreach ($row as $field => &$value) {
+            foreach ($result as $rowKey => $row) {
+                foreach ($row as $field => $value) {
                     if (isset($definition['fields'][$field])) {
-                        $value = ObjectModel::formatValue(
+                        $value[$rowKey][$field] = ObjectModel::formatValue(
                             $value,
                             $definition['fields'][$field]['type'],
                             false,

controllers/admin/AdminShopController.php
@@ -137,19 +137,19 @@ class AdminShopControllerCore extends AdminController
         $this->addJqueryPlugin('cooki-plugin');
         $data = Shop::getTree();
 
-        foreach ($data as &$group) {
-            foreach ($group['shops'] as &$shop) {
+        foreach ($data as $group_key => $group) {
+            foreach ($group['shops'] as $shop_key => $shop) {
                 $current_shop = new Shop($shop['id_shop']);
                 $urls = $current_shop->getUrls();
 
-                foreach ($urls as &$url) {
+                foreach ($urls as $url) {
                     $title = $url['domain'] . $url['physical_uri'] . $url['virtual_uri'];
                     if (strlen($title) > 23) {
                         $title = substr($title, 0, 23) . '...';
                     }
 
                     $url['name'] = $title;
-                    $shop['urls'][$url['id_shop_url']] = $url;
+                    $shop[$group_key][$shop_key]['urls'][$url['id_shop_url']] = $url;
                 }
             }
         }
@@ -344,7 +344,7 @@ class AdminShopControllerCore extends AdminController
         $shop_delete_list = [];
 
         // don't allow to remove shop which have dependencies (customers / orders / ... )
-        foreach ($this->_list as &$shop) {
+        foreach ($this->_list as $shop) {
             if (Shop::hasDependency($shop['id_shop'])) {
                 $shop_delete_list[] = $shop['id_shop'];
             }

controllers/admin/AdminShopUrlController.php
@@ -347,19 +347,19 @@ class AdminShopUrlControllerCore extends AdminController
         $this->addJqueryPlugin('cooki-plugin');
         $data = Shop::getTree();
 
-        foreach ($data as &$group) {
-            foreach ($group['shops'] as &$shop) {
+        foreach ($data as $group_key => $group) {
+            foreach ($group['shops'] as $shop_key => $shop) {
                 $current_shop = new Shop($shop['id_shop']);
                 $urls = $current_shop->getUrls();
 
-                foreach ($urls as &$url) {
+                foreach ($urls as $url) {
                     $title = $url['domain'] . $url['physical_uri'] . $url['virtual_uri'];
                     if (strlen($title) > 23) {
                         $title = substr($title, 0, 23) . '...';
                     }
 
                     $url['name'] = $title;
-                    $shop['urls'][$url['id_shop_url']] = $url;
+                    $shop[$group_key][$shop_key]['urls'][$url['id_shop_url']] = $url;
                 }
             }
         }

leedzUp/leedzup

classes/ObjectModel.php
@@ -661,10 +661,10 @@ abstract class ObjectModelCore implements \PrestaShop\PrestaShop\Core\Foundation
                 return false;
             }
 
-            foreach ($result as &$row) {
-                foreach ($row as $field => &$value) {
+            foreach ($result as $rowKey => $row) {
+                foreach ($row as $field => $value) {
                     if (isset($definition['fields'][$field])) {
-                        $value = ObjectModel::formatValue(
+                        $result[$rowKey][$field] = ObjectModel::formatValue(
                             $value,
                             $definition['fields'][$field]['type'],
                             false,

controllers/admin/AdminShopController.php
@@ -137,19 +137,19 @@ class AdminShopControllerCore extends AdminController
         $this->addJqueryPlugin('cooki-plugin');
         $data = Shop::getTree();
 
-        foreach ($data as &$group) {
-            foreach ($group['shops'] as &$shop) {
+        foreach ($data as $group_key => $group) {
+            foreach ($group['shops'] as $shop_key => $shop) {
                 $current_shop = new Shop($shop['id_shop']);
                 $urls = $current_shop->getUrls();
 
-                foreach ($urls as &$url) {
+                foreach ($urls as $url) {
                     $title = $url['domain'] . $url['physical_uri'] . $url['virtual_uri'];
                     if (strlen($title) > 23) {
                         $title = substr($title, 0, 23) . '...';
                     }
 
                     $url['name'] = $title;
-                    $shop['urls'][$url['id_shop_url']] = $url;
+                    $data[$group_key][$shop_key]['urls'][$url['id_shop_url']] = $url;
                 }
             }
         }
@@ -344,7 +344,7 @@ class AdminShopControllerCore extends AdminController
         $shop_delete_list = [];
 
         // don't allow to remove shop which have dependencies (customers / orders / ... )
-        foreach ($this->_list as &$shop) {
+        foreach ($this->_list as $shop) {
             if (Shop::hasDependency($shop['id_shop'])) {
                 $shop_delete_list[] = $shop['id_shop'];
             }

controllers/admin/AdminShopUrlController.php
@@ -347,19 +347,19 @@ class AdminShopUrlControllerCore extends AdminController
         $this->addJqueryPlugin('cooki-plugin');
         $data = Shop::getTree();
 
-        foreach ($data as &$group) {
-            foreach ($group['shops'] as &$shop) {
+        foreach ($data as $group_key => $group) {
+            foreach ($group['shops'] as $shop_key => $shop) {
                 $current_shop = new Shop($shop['id_shop']);
                 $urls = $current_shop->getUrls();
 
-                foreach ($urls as &$url) {
+                foreach ($urls as $url) {
                     $title = $url['domain'] . $url['physical_uri'] . $url['virtual_uri'];
                     if (strlen($title) > 23) {
                         $title = substr($title, 0, 23) . '...';
                     }
 
                     $url['name'] = $title;
-                    $shop['urls'][$url['id_shop_url']] = $url;
+                    $data[$group_key][$shop_key]['urls'][$url['id_shop_url']] = $url;
                 }
             }
         }