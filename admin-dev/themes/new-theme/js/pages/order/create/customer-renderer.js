PrestaShop/PrestaShop

Manage fancybox for cart, order and customer details. Add some loading info during ajax calls

admin-dev/themes/new-theme/js/pages/order/create/customer-renderer.js
@@ -64,6 +64,13 @@ export default class CustomerRenderer {
 
       this._renderFoundCustomer(customer);
     }
+
+    // Show customer details in fancy box
+    $(createOrderMap.customerDetailsBtn).fancybox({
+      'type': 'iframe',
+      'width': '90%',
+      'height': '90%',
+    });
   }
 
   /**
@@ -86,6 +93,11 @@ export default class CustomerRenderer {
       .closest(createOrderMap.customerSearchResultColumn)
       .remove()
     ;
+
+    // Initial display of the customer, the cart is gonna be created then customer's carts
+    // and orders are going to be fetched, but we can display the loading messages right now
+    this.showLoadingCarts();
+    this.showLoadingOrders();
   }
 
   /**
@@ -93,6 +105,15 @@ export default class CustomerRenderer {
    */
   showCustomerSearch() {
     this.$container.find(createOrderMap.customerSearchRow).removeClass('d-none');
+  }
+
+  /**
+   * Empty the cart list and display a loading message.
+   */
+  showLoadingCarts() {
+    const $cartsTable = $(createOrderMap.customerCartsTable);
+    $cartsTable.find('tbody').empty();
+    this._renderLoading($cartsTable);
   }
 
   /**
@@ -129,7 +150,10 @@ export default class CustomerRenderer {
       $cartsTableRow.find(createOrderMap.cartTotalField).text(cart.totalPrice);
       $cartsTableRow.find(createOrderMap.cartDetailsBtn).prop(
         'href',
-        this.router.generate('admin_carts_view', {cartId: cart.cartId}),
+        this.router.generate('admin_carts_view', {
+          cartId: cart.cartId,
+          liteDisplaying: 1,
+        }),
       );
 
       $cartsTableRow.find(createOrderMap.useCartBtn).data('cart-id', cart.cartId);
@@ -137,6 +161,22 @@ export default class CustomerRenderer {
       $cartsTable.find('thead').removeClass('d-none');
       $cartsTable.find('tbody').append($cartsTableRow);
     }
+
+    // Show cart details in fancy box
+    $(createOrderMap.cartDetailsBtn).fancybox({
+      'type': 'iframe',
+      'width': '90%',
+      'height': '90%',
+    });
+  }
+
+  /**
+   * Empty the order list and display a loading message.
+   */
+  showLoadingOrders() {
+    const $ordersTable = $(createOrderMap.customerOrdersTable);
+    $ordersTable.find('tbody').empty();
+    this._renderLoading($ordersTable);
   }
 
   /**
@@ -171,7 +211,10 @@ export default class CustomerRenderer {
       $template.find(createOrderMap.orderStatusField).text(order.orderStatus);
       $template.find(createOrderMap.orderDetailsBtn).prop(
         'href',
-        this.router.generate('admin_orders_view', {orderId: order.orderId}),
+        this.router.generate('admin_orders_view', {
+          orderId: order.orderId,
+          liteDisplaying: 1
+        }),
       );
 
       $template.find(createOrderMap.useOrderBtn).data('order-id', order.orderId);
@@ -179,6 +222,13 @@ export default class CustomerRenderer {
       $ordersTable.find('thead').removeClass('d-none');
       $ordersTable.find('tbody').append($template);
     }
+
+    // Show order details in fancy box
+    $(createOrderMap.orderDetailsBtn).fancybox({
+      'type': 'iframe',
+      'width': '90%',
+      'height': '90%',
+    });
   }
 
   /**
@@ -212,6 +262,20 @@ export default class CustomerRenderer {
   }
 
   /**
+   * Shows searching customers notice during request
+   */
+  showSearchingCustomers() {
+    $(createOrderMap.customerSearchLoadingNotice).removeClass('d-none');
+  }
+
+  /**
+   * Hide searching notice
+   */
+  hideSearchingCustomers() {
+    $(createOrderMap.customerSearchLoadingNotice).addClass('d-none');
+  }
+
+  /**
    * Renders 'No records' warning in list
    *
    * @param $table
@@ -220,6 +284,18 @@ export default class CustomerRenderer {
    */
   _renderEmptyList($table) {
     const $emptyTableRow = $($(createOrderMap.emptyListRowTemplate).html()).clone();
+    $table.find('tbody').append($emptyTableRow);
+  }
+
+  /**
+   * Renders 'Loading' message in list
+   *
+   * @param $table
+   *
+   * @private
+   */
+  _renderLoading($table) {
+    const $emptyTableRow = $($(createOrderMap.loadingListRowTemplate).html()).clone();
     $table.find('tbody').append($emptyTableRow);
   }
 
@@ -252,7 +328,10 @@ export default class CustomerRenderer {
     $template.find(createOrderMap.chooseCustomerBtn).data('customer-id', customer.id);
     $template.find(createOrderMap.customerDetailsBtn).prop(
       'href',
-      this.router.generate('admin_customers_view', {customerId: customer.id}),
+      this.router.generate('admin_customers_view', {
+        customerId: customer.id,
+        liteDisplaying: 1
+      }),
     );
 
     return this.$customerSearchResultBlock.append($template);

leedzUp/leedzup

admin-dev/themes/new-theme/js/pages/order/create/customer-renderer.js
@@ -64,6 +64,13 @@ export default class CustomerRenderer {
 
       this._renderFoundCustomer(customer);
     }
+
+    // Show customer details in fancy box
+    $(createOrderMap.customerDetailsBtn).fancybox({
+      'type': 'iframe',
+      'width': '90%',
+      'height': '90%',
+    });
   }
 
   /**
@@ -86,6 +93,11 @@ export default class CustomerRenderer {
       .closest(createOrderMap.customerSearchResultColumn)
       .remove()
     ;
+
+    // Initial display of the customer, the cart is gonna be created then customer's carts
+    // and orders are going to be fetched, but we can display the loading messages right now
+    this.showLoadingCarts();
+    this.showLoadingOrders();
   }
 
   /**
@@ -93,6 +105,15 @@ export default class CustomerRenderer {
    */
   showCustomerSearch() {
     this.$container.find(createOrderMap.customerSearchRow).removeClass('d-none');
+  }
+
+  /**
+   * Empty the cart list and display a loading message.
+   */
+  showLoadingCarts() {
+    const $cartsTable = $(createOrderMap.customerCartsTable);
+    $cartsTable.find('tbody').empty();
+    this.renderLoading($cartsTable);
   }
 
   /**
@@ -129,7 +150,10 @@ export default class CustomerRenderer {
       $cartsTableRow.find(createOrderMap.cartTotalField).text(cart.totalPrice);
       $cartsTableRow.find(createOrderMap.cartDetailsBtn).prop(
         'href',
-        this.router.generate('admin_carts_view', {cartId: cart.cartId}),
+        this.router.generate('admin_carts_view', {
+          cartId: cart.cartId,
+          liteDisplaying: 1,
+        }),
       );
 
       $cartsTableRow.find(createOrderMap.useCartBtn).data('cart-id', cart.cartId);
@@ -137,6 +161,22 @@ export default class CustomerRenderer {
       $cartsTable.find('thead').removeClass('d-none');
       $cartsTable.find('tbody').append($cartsTableRow);
     }
+
+    // Show cart details in fancy box
+    $(createOrderMap.cartDetailsBtn).fancybox({
+      'type': 'iframe',
+      'width': '90%',
+      'height': '90%',
+    });
+  }
+
+  /**
+   * Empty the order list and display a loading message.
+   */
+  showLoadingOrders() {
+    const $ordersTable = $(createOrderMap.customerOrdersTable);
+    $ordersTable.find('tbody').empty();
+    this.renderLoading($ordersTable);
   }
 
   /**
@@ -171,7 +211,10 @@ export default class CustomerRenderer {
       $template.find(createOrderMap.orderStatusField).text(order.orderStatus);
       $template.find(createOrderMap.orderDetailsBtn).prop(
         'href',
-        this.router.generate('admin_orders_view', {orderId: order.orderId}),
+        this.router.generate('admin_orders_view', {
+          orderId: order.orderId,
+          liteDisplaying: 1
+        }),
       );
 
       $template.find(createOrderMap.useOrderBtn).data('order-id', order.orderId);
@@ -179,6 +222,13 @@ export default class CustomerRenderer {
       $ordersTable.find('thead').removeClass('d-none');
       $ordersTable.find('tbody').append($template);
     }
+
+    // Show order details in fancy box
+    $(createOrderMap.orderDetailsBtn).fancybox({
+      'type': 'iframe',
+      'width': '90%',
+      'height': '90%',
+    });
   }
 
   /**
@@ -212,6 +262,20 @@ export default class CustomerRenderer {
   }
 
   /**
+   * Shows searching customers notice during request
+   */
+  showSearchingCustomers() {
+    $(createOrderMap.customerSearchLoadingNotice).removeClass('d-none');
+  }
+
+  /**
+   * Hide searching notice
+   */
+  hideSearchingCustomers() {
+    $(createOrderMap.customerSearchLoadingNotice).addClass('d-none');
+  }
+
+  /**
    * Renders 'No records' warning in list
    *
    * @param $table
@@ -220,6 +284,18 @@ export default class CustomerRenderer {
    */
   _renderEmptyList($table) {
     const $emptyTableRow = $($(createOrderMap.emptyListRowTemplate).html()).clone();
+    $table.find('tbody').append($emptyTableRow);
+  }
+
+  /**
+   * Renders 'Loading' message in list
+   *
+   * @param $table
+   *
+   * @private
+   */
+  renderLoading($table) {
+    const $emptyTableRow = $($(createOrderMap.loadingListRowTemplate).html()).clone();
     $table.find('tbody').append($emptyTableRow);
   }
 
@@ -252,7 +328,10 @@ export default class CustomerRenderer {
     $template.find(createOrderMap.chooseCustomerBtn).data('customer-id', customer.id);
     $template.find(createOrderMap.customerDetailsBtn).prop(
       'href',
-      this.router.generate('admin_customers_view', {customerId: customer.id}),
+      this.router.generate('admin_customers_view', {
+        customerId: customer.id,
+        liteDisplaying: 1
+      }),
     );
 
     return this.$customerSearchResultBlock.append($template);