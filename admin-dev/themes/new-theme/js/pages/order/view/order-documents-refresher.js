PrestaShop/PrestaShop

Refresh Documents tab when adding a product in order view

admin-dev/themes/new-theme/js/pages/order/view/order-documents-refresher.js
@@ -1,0 +1,46 @@
+/**
+ * 2007-2020 PrestaShop SA and Contributors
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2020 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+import Router from '@components/router';
+import OrderViewPageMap from '@pages/order/OrderViewPageMap';
+import InvoiceNoteManager from '../invoice-note-manager';
+
+const $ = window.$;
+
+export default class OrderDocumentsRefresher {
+  constructor() {
+    this.router = new Router();
+    this.invoiceNoteManager = new InvoiceNoteManager();
+  }
+
+  refresh(orderId) {
+    $.ajax(this.router.generate('admin_orders_get_documents', {orderId}))
+      .then((response) => {
+        $(OrderViewPageMap.orderDocumentsTabCount).text(response.total);
+        $(OrderViewPageMap.orderDocumentsTabContentTable).html(response.html);
+        this.invoiceNoteManager.setupListeners();
+      });
+  }
+}

leedzUp/leedzup

admin-dev/themes/new-theme/js/pages/order/view/order-documents-refresher.js
@@ -1,0 +1,46 @@
+/**
+ * 2007-2020 PrestaShop SA and Contributors
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2020 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+import Router from '@components/router';
+import OrderViewPageMap from '@pages/order/OrderViewPageMap';
+import InvoiceNoteManager from '../invoice-note-manager';
+
+const $ = window.$;
+
+export default class OrderDocumentsRefresher {
+  constructor() {
+    this.router = new Router();
+    this.invoiceNoteManager = new InvoiceNoteManager();
+  }
+
+  refresh(orderId) {
+    $.ajax(this.router.generate('admin_orders_get_documents', {orderId}))
+      .then((response) => {
+        $(OrderViewPageMap.orderDocumentsTabCount).text(response.total);
+        $(OrderViewPageMap.orderDocumentsTabBody).html(response.html);
+        this.invoiceNoteManager.setupListeners();
+      });
+  }
+}