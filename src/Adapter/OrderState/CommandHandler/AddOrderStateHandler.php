PrestaShop/PrestaShop

Rename logable to loggable

src/Adapter/OrderState/CommandHandler/AddOrderStateHandler.php
@@ -63,7 +63,7 @@ final class AddOrderStateHandler extends AbstractOrderStateHandler implements Ad
     {
         $orderState->name = $command->getLocalizedNames();
         $orderState->color = $command->getColor();
-        $orderState->logable = $command->isLogable();
+        $orderState->loggable = $command->isLoggable();
         $orderState->invoice = $command->isInvoice();
         $orderState->hidden = $command->isHidden();
         $orderState->send_email = $command->isSendEmail();

src/Adapter/OrderState/CommandHandler/EditOrderStateHandler.php
@@ -91,8 +91,8 @@ final class EditOrderStateHandler extends AbstractOrderStateHandler implements E
             $orderState->color = $command->getColor();
         }
 
-        if (null !== $command->isLogable()) {
-            $orderState->logable = $command->isLogable();
+        if (null !== $command->isLoggable()) {
+            $orderState->loggable = $command->isLoggable();
         }
 
         if (null !== $command->isHidden()) {

leedzUp/leedzup

src/Adapter/OrderState/CommandHandler/AddOrderStateHandler.php
@@ -63,7 +63,7 @@ final class AddOrderStateHandler extends AbstractOrderStateHandler implements Ad
     {
         $orderState->name = $command->getLocalizedNames();
         $orderState->color = $command->getColor();
-        $orderState->logable = $command->isLogable();
+        $orderState->logable = $command->isLoggable();
         $orderState->invoice = $command->isInvoice();
         $orderState->hidden = $command->isHidden();
         $orderState->send_email = $command->isSendEmail();

src/Adapter/OrderState/CommandHandler/EditOrderStateHandler.php
@@ -91,8 +91,8 @@ final class EditOrderStateHandler extends AbstractOrderStateHandler implements E
             $orderState->color = $command->getColor();
         }
 
-        if (null !== $command->isLogable()) {
-            $orderState->logable = $command->isLogable();
+        if (null !== $command->isLoggable()) {
+            $orderState->logable = $command->isLoggable();
         }
 
         if (null !== $command->isHidden()) {