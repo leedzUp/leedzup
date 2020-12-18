PrestaShop/PrestaShop

implement CombinationStock DTO in query handler

src/Adapter/Product/QueryHandler/GetCombinationForEditingHandler.php
@@ -29,11 +29,18 @@ declare(strict_types=1);
 namespace PrestaShop\PrestaShop\Adapter\Product\QueryHandler;
 
 use Combination;
+use DateTime;
 use PrestaShop\PrestaShop\Adapter\Product\Repository\CombinationRepository;
+use PrestaShop\PrestaShop\Adapter\Product\Repository\StockAvailableRepository;
+use PrestaShop\PrestaShop\Core\Domain\Product\Combination\Exception\CombinationConstraintException;
 use PrestaShop\PrestaShop\Core\Domain\Product\Combination\Query\GetCombinationForEditing;
 use PrestaShop\PrestaShop\Core\Domain\Product\Combination\QueryHandler\GetCombinationForEditingHandlerInterface;
 use PrestaShop\PrestaShop\Core\Domain\Product\Combination\QueryResult\CombinationForEditing;
 use PrestaShop\PrestaShop\Core\Domain\Product\Combination\QueryResult\CombinationOptions;
+use PrestaShop\PrestaShop\Core\Domain\Product\Combination\QueryResult\CombinationStock;
+use PrestaShop\PrestaShop\Core\Domain\Product\Combination\ValueObject\CombinationId;
+use PrestaShop\PrestaShop\Core\Domain\Product\Stock\Exception\StockAvailableNotFoundException;
+use PrestaShop\PrestaShop\Core\Exception\CoreException;
 
 /**
  * Handles @see GetCombinationForEditing query using legacy object model
@@ -46,12 +53,20 @@ final class GetCombinationForEditingHandler implements GetCombinationForEditingH
     private $combinationRepository;
 
     /**
+     * @var StockAvailableRepository
+     */
+    private $stockAvailableRepository;
+
+    /**
      * @param CombinationRepository $combinationRepository
+     * @param StockAvailableRepository $stockAvailableRepository
      */
     public function __construct(
-        CombinationRepository $combinationRepository
+        CombinationRepository $combinationRepository,
+        StockAvailableRepository $stockAvailableRepository
     ) {
         $this->combinationRepository = $combinationRepository;
+        $this->stockAvailableRepository = $stockAvailableRepository;
     }
 
     /**
@@ -62,7 +77,8 @@ final class GetCombinationForEditingHandler implements GetCombinationForEditingH
         $combination = $this->combinationRepository->get($query->getCombinationId());
 
         return new CombinationForEditing(
-            $this->getOptions($combination)
+            $this->getOptions($combination),
+            $this->getStock($combination)
         );
     }
 
@@ -79,6 +95,29 @@ final class GetCombinationForEditingHandler implements GetCombinationForEditingH
             $combination->mpn,
             $combination->reference,
             $combination->upc
+        );
+    }
+
+    /**
+     * @param Combination $combination
+     *
+     * @return CombinationStock
+     *
+     * @throws CombinationConstraintException
+     * @throws StockAvailableNotFoundException
+     * @throws CoreException
+     */
+    private function getStock(Combination $combination): CombinationStock
+    {
+        $stockAvailable = $this->stockAvailableRepository->getForCombination(new Combinationid($combination->id));
+
+        return new CombinationStock(
+            (int) $stockAvailable->quantity,
+            $combination->minimal_quantity,
+            $combination->location,
+            $combination->low_stock_threshold,
+            (bool) $combination->low_stock_alert,
+            new DateTime($combination->available_date)
         );
     }
 }

src/Core/Domain/Product/Combination/QueryResult/CombinationForEditing.php
@@ -39,12 +39,20 @@ class CombinationForEditing
     private $options;
 
     /**
+     * @var CombinationStock
+     */
+    private $stock;
+
+    /**
      * @param CombinationOptions $options
+     * @param CombinationStock $stock
      */
     public function __construct(
-        CombinationOptions $options
+        CombinationOptions $options,
+        CombinationStock $stock
     ) {
         $this->options = $options;
+        $this->stock = $stock;
     }
 
     /**
@@ -53,5 +61,13 @@ class CombinationForEditing
     public function getOptions(): CombinationOptions
     {
         return $this->options;
+    }
+
+    /**
+     * @return CombinationStock
+     */
+    public function getStock(): CombinationStock
+    {
+        return $this->stock;
     }
 }

leedzUp/leedzup

src/Adapter/Product/QueryHandler/GetCombinationForEditingHandler.php
@@ -29,13 +29,17 @@ declare(strict_types=1);
 namespace PrestaShop\PrestaShop\Adapter\Product\QueryHandler;
 
 use Combination;
+use DateTime;
 use PrestaShop\Decimal\DecimalNumber;
 use PrestaShop\PrestaShop\Adapter\Product\Repository\CombinationRepository;
+use PrestaShop\PrestaShop\Adapter\Product\Repository\StockAvailableRepository;
 use PrestaShop\PrestaShop\Core\Domain\Product\Combination\Query\GetCombinationForEditing;
 use PrestaShop\PrestaShop\Core\Domain\Product\Combination\QueryHandler\GetCombinationForEditingHandlerInterface;
 use PrestaShop\PrestaShop\Core\Domain\Product\Combination\QueryResult\CombinationDetails;
 use PrestaShop\PrestaShop\Core\Domain\Product\Combination\QueryResult\CombinationForEditing;
 use PrestaShop\PrestaShop\Core\Domain\Product\Combination\QueryResult\CombinationPrices;
+use PrestaShop\PrestaShop\Core\Domain\Product\Combination\QueryResult\CombinationStock;
+use PrestaShop\PrestaShop\Core\Domain\Product\Combination\ValueObject\CombinationId;
 
 /**
  * Handles @see GetCombinationForEditing query using legacy object model
@@ -48,12 +52,20 @@ final class GetCombinationForEditingHandler implements GetCombinationForEditingH
     private $combinationRepository;
 
     /**
+     * @var StockAvailableRepository
+     */
+    private $stockAvailableRepository;
+
+    /**
      * @param CombinationRepository $combinationRepository
+     * @param StockAvailableRepository $stockAvailableRepository
      */
     public function __construct(
-        CombinationRepository $combinationRepository
+        CombinationRepository $combinationRepository,
+        StockAvailableRepository $stockAvailableRepository
     ) {
         $this->combinationRepository = $combinationRepository;
+        $this->stockAvailableRepository = $stockAvailableRepository;
     }
 
     /**
@@ -65,7 +77,8 @@ final class GetCombinationForEditingHandler implements GetCombinationForEditingH
 
         return new CombinationForEditing(
             $this->getDetails($combination),
-            $this->getPrices($combination)
+            $this->getPrices($combination),
+            $this->getStock($combination)
         );
     }
 
@@ -98,6 +111,25 @@ final class GetCombinationForEditingHandler implements GetCombinationForEditingH
             new DecimalNumber($combination->price),
             new DecimalNumber($combination->unit_price_impact),
             new DecimalNumber($combination->wholesale_price)
+        );
+    }
+
+    /**
+     * @param Combination $combination
+     *
+     * @return CombinationStock
+     */
+    private function getStock(Combination $combination): CombinationStock
+    {
+        $stockAvailable = $this->stockAvailableRepository->getForCombination(new Combinationid($combination->id));
+
+        return new CombinationStock(
+            (int) $stockAvailable->quantity,
+            $combination->minimal_quantity,
+            $combination->location,
+            $combination->low_stock_threshold,
+            (bool) $combination->low_stock_alert,
+            new DateTime($combination->available_date)
         );
     }
 }

src/Core/Domain/Product/Combination/QueryResult/CombinationForEditing.php
@@ -44,14 +44,22 @@ class CombinationForEditing
     private $prices;
 
     /**
-     * @param CombinationDetails $details
+     * @var CombinationStock
+     */
+    private $stock;
+
+    /**
+     * @param CombinationDetails $options
      * @param CombinationPrices $prices
+     * @param CombinationStock $stock
      */
     public function __construct(
-        CombinationDetails $details,
-        CombinationPrices $prices
+        CombinationDetails $options,
+        CombinationPrices $prices,
+        CombinationStock $stock
     ) {
-        $this->details = $details;
+        $this->details = $options;
+        $this->stock = $stock;
         $this->prices = $prices;
     }
 
@@ -69,5 +77,13 @@ class CombinationForEditing
     public function getPrices(): CombinationPrices
     {
         return $this->prices;
+    }
+
+    /**
+     * @return CombinationStock
+     */
+    public function getStock(): CombinationStock
+    {
+        return $this->stock;
     }
 }