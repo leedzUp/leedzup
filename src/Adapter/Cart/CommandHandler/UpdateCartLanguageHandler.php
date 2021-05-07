PrestaShop/PrestaShop

Fixed errors on PHPStan Level 5

src/Adapter/Cart/CommandHandler/UpdateCartLanguageHandler.php
@@ -75,7 +75,10 @@ final class UpdateCartLanguageHandler extends AbstractCartHandler implements Upd
         try {
             $lang = new Language($languageId->getValue());
         } catch (PrestaShopException $e) {
-            throw new LanguageException($languageId, sprintf('An error occurred when fetching language object with id %s', $languageId->getValue()));
+            throw new LanguageException(
+                sprintf('An error occurred when fetching language object with id %s', $languageId->getValue()),
+                $languageId->getValue()
+            );
         }
 
         if ($languageId->getValue() !== $lang->id) {

src/Adapter/Category/CommandHandler/DeleteCategoryMenuThumbnailImageHandler.php
@@ -84,7 +84,10 @@ final class DeleteCategoryMenuThumbnailImageHandler implements DeleteCategoryMen
         $category = new Category($categoryId->getValue());
 
         if ($category->id !== $categoryId->getValue()) {
-            throw new CategoryNotFoundException($categoryId, sprintf('Category with id "%s" was not found', $categoryId));
+            throw new CategoryNotFoundException(
+                $categoryId,
+                sprintf('Category with id "%s" was not found', $categoryId->getValue())
+            );
         }
 
         $thumbnailPath = sprintf(

leedzUp/leedzup

src/Adapter/Cart/CommandHandler/UpdateCartLanguageHandler.php
@@ -75,7 +75,10 @@ final class UpdateCartLanguageHandler extends AbstractCartHandler implements Upd
         try {
             $lang = new Language($languageId->getValue());
         } catch (PrestaShopException $e) {
-            throw new LanguageException($languageId, sprintf('An error occurred when fetching language object with id %s', $languageId->getValue()));
+            throw new LanguageException(
+                sprintf('An error occurred when fetching language object with id %d', $languageId->getValue()),
+                $languageId->getValue()
+            );
         }
 
         if ($languageId->getValue() !== $lang->id) {

src/Adapter/Category/CommandHandler/DeleteCategoryMenuThumbnailImageHandler.php
@@ -84,7 +84,10 @@ final class DeleteCategoryMenuThumbnailImageHandler implements DeleteCategoryMen
         $category = new Category($categoryId->getValue());
 
         if ($category->id !== $categoryId->getValue()) {
-            throw new CategoryNotFoundException($categoryId, sprintf('Category with id "%s" was not found', $categoryId));
+            throw new CategoryNotFoundException(
+                $categoryId,
+                sprintf('Category with id "%d" was not found', $categoryId->getValue())
+            );
         }
 
         $thumbnailPath = sprintf(