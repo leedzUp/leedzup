PrestaShop/PrestaShop

Added support for WebP

install-dev/upgrade/sql/1.7.9.0.sql
@@ -5,7 +5,8 @@ INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VAL
     ('PS_MAIL_DKIM_ENABLE', '0', NOW(), NOW()),
     ('PS_MAIL_DKIM_DOMAIN', '', NOW(), NOW()),
     ('PS_MAIL_DKIM_SELECTOR', '', NOW(), NOW()),
-    ('PS_MAIL_DKIM_KEY', '', NOW(), NOW())
+    ('PS_MAIL_DKIM_KEY', '', NOW(), NOW()),
+    ('PS_WEB_PQUALITY', '80', NOW(), NOW())
 ;
 INSERT IGNORE INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `position`) VALUES
   (NULL, 'actionValidateOrderAfter', 'New Order', 'This hook is called after validating an order by core', '1');

src/Core/Domain/Shop/DTO/ShopLogoSettings.php
@@ -40,7 +40,12 @@ class ShopLogoSettings
     public const FAVICON_FILE_NAME = 'PS_FAVICON';
 
     /**
-     * @var array - a list of available image mime types
+     * @var array<int, string> List of available image mime types
+     */
+    public const AVAILABLE_HEADER_LOGO_IMAGE_EXTENSIONS = ['gif', 'jpg', 'jpeg', 'jpe', 'png', 'webp'];
+
+    /**
+     * @var array<int, string> List of available image mime types
      */
     public const AVAILABLE_LOGO_IMAGE_EXTENSIONS = ['gif', 'jpg', 'jpeg', 'jpe', 'png'];
 
@@ -52,9 +57,24 @@ class ShopLogoSettings
     /**
      * Gets the list of available extensions with dot attached to the front of the extension
      *
-     * @return array
+     * @return array<int, string>
      */
-    public function getLogoImageExtensionsWithDot()
+    public function getHeaderLogoImageExtensionsWithDot()
+    {
+        $mimeTypes = [];
+        foreach (self::AVAILABLE_HEADER_LOGO_IMAGE_EXTENSIONS as $imageExtension) {
+            $mimeTypes[] = '.' . $imageExtension;
+        }
+
+        return $mimeTypes;
+    }
+
+    /**
+     * Gets the list of available extensions with dot attached to the front of the extension
+     *
+     * @return array<int, string>
+     */
+    public function getLogoImageExtensionsWithDot(): array
     {
         $mimeTypes = [];
         foreach (self::AVAILABLE_LOGO_IMAGE_EXTENSIONS as $imageExtension) {

leedzUp/leedzup

install-dev/upgrade/sql/1.7.9.0.sql
@@ -5,7 +5,8 @@ INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VAL
     ('PS_MAIL_DKIM_ENABLE', '0', NOW(), NOW()),
     ('PS_MAIL_DKIM_DOMAIN', '', NOW(), NOW()),
     ('PS_MAIL_DKIM_SELECTOR', '', NOW(), NOW()),
-    ('PS_MAIL_DKIM_KEY', '', NOW(), NOW())
+    ('PS_MAIL_DKIM_KEY', '', NOW(), NOW()),
+    ('PS_WEBP_QUALITY', '80', NOW(), NOW())
 ;
 INSERT IGNORE INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `position`) VALUES
   (NULL, 'actionValidateOrderAfter', 'New Order', 'This hook is called after validating an order by core', '1');

src/Core/Domain/Shop/DTO/ShopLogoSettings.php
@@ -40,9 +40,9 @@ class ShopLogoSettings
     public const FAVICON_FILE_NAME = 'PS_FAVICON';
 
     /**
-     * @var array - a list of available image mime types
+     * @var array<int, string> List of available image mime types
      */
-    public const AVAILABLE_LOGO_IMAGE_EXTENSIONS = ['gif', 'jpg', 'jpeg', 'jpe', 'png'];
+    public const AVAILABLE_LOGO_IMAGE_EXTENSIONS = ['gif', 'jpg', 'jpeg', 'jpe', 'png', 'webp'];
 
     /**
      * @var string - a type used for icon images for shop logo
@@ -52,7 +52,7 @@ class ShopLogoSettings
     /**
      * Gets the list of available extensions with dot attached to the front of the extension
      *
-     * @return array
+     * @return array<int, string>
      */
     public function getLogoImageExtensionsWithDot()
     {