PrestaShop/PrestaShop

Render error when accessing a product in a shop not associated

src/PrestaShopBundle/Controller/Admin/Sell/Catalog/Product/ProductController.php
@@ -35,6 +35,7 @@ use PrestaShop\PrestaShop\Core\Domain\Product\Exception\CannotDeleteProductExcep
 use PrestaShop\PrestaShop\Core\Domain\Product\Exception\ProductConstraintException;
 use PrestaShop\PrestaShop\Core\Domain\Product\FeatureValue\Exception\DuplicateFeatureValueAssociationException;
 use PrestaShop\PrestaShop\Core\Domain\Product\FeatureValue\Exception\InvalidAssociatedFeatureException;
+use PrestaShop\PrestaShop\Core\Domain\Shop\Exception\ShopAssociationNotFound;
 use PrestaShop\PrestaShop\Core\Exception\ProductException;
 use PrestaShop\PrestaShop\Core\FeatureFlag\FeatureFlagSettings;
 use PrestaShop\PrestaShop\Core\Form\IdentifiableObject\Builder\FormBuilderInterface;
@@ -152,12 +153,16 @@ class ProductController extends FrameworkBundleAdminController
             return $this->renderDisableMultistorePage($productId);
         }
 
-        $productForm = $this->getProductFormBuilder()->getFormFor($productId, [], [
-            'product_id' => $productId,
-            // @todo: patch/partial update doesn't work good for now (especially multiple empty values) so we use POST for now
-            // 'method' => Request::METHOD_PATCH,
-            'method' => Request::METHOD_POST,
-        ]);
+        try {
+            $productForm = $this->getProductFormBuilder()->getFormFor($productId, [], [
+                'product_id' => $productId,
+                // @todo: patch/partial update doesn't work good for now (especially multiple empty values) so we use POST for now
+                // 'method' => Request::METHOD_PATCH,
+                'method' => Request::METHOD_POST,
+            ]);
+        } catch (ShopAssociationNotFound $e) {
+            return $this->renderMissingAssociation($productId);
+        }
 
         try {
             $productForm->handleRequest($request);
@@ -365,6 +370,26 @@ class ProductController extends FrameworkBundleAdminController
                 ]
             )
         );
+    }
+
+    /**
+     * @param int $productId
+     *
+     * @return Response
+     */
+    private function renderMissingAssociation(int $productId): Response
+    {
+        //@todo this error message should be improved to indicate which shop can/should be used for this product and/or how to associate it to the current context
+        return $this->render('@PrestaShop/Admin/Sell/Catalog/Product/disabled.html.twig', [
+            'errorMessage' => $this->trans(
+                'This product is not associated to the store selected in the current context, please select another one.',
+                'Admin.Notifications.Info',
+            ),
+            'standardPageUrl' => $this->generateUrl(
+                !empty($productId) ? 'admin_product_form' : 'admin_product_new',
+                !empty($productId) ? ['id' => $productId] : []
+            ),
+        ]);
     }
 
     /**

leedzUp/leedzup

src/PrestaShopBundle/Controller/Admin/Sell/Catalog/Product/ProductController.php
@@ -37,6 +37,7 @@ use PrestaShop\PrestaShop\Core\Domain\Product\FeatureValue\Exception\DuplicateFe
 use PrestaShop\PrestaShop\Core\Domain\Product\FeatureValue\Exception\InvalidAssociatedFeatureException;
 use PrestaShop\PrestaShop\Core\Domain\Product\Query\SearchProductsForAssociation;
 use PrestaShop\PrestaShop\Core\Domain\Product\QueryResult\ProductForAssociation;
+use PrestaShop\PrestaShop\Core\Domain\Shop\Exception\ShopAssociationNotFound;
 use PrestaShop\PrestaShop\Core\Exception\ProductException;
 use PrestaShop\PrestaShop\Core\FeatureFlag\FeatureFlagSettings;
 use PrestaShop\PrestaShop\Core\Form\IdentifiableObject\Builder\FormBuilderInterface;
@@ -155,12 +156,16 @@ class ProductController extends FrameworkBundleAdminController
             return $this->renderDisableMultistorePage($productId);
         }
 
-        $productForm = $this->getEditProductFormBuilder()->getFormFor($productId, [], [
-            'product_id' => $productId,
-            // @todo: patch/partial update doesn't work good for now (especially multiple empty values) so we use POST for now
-            // 'method' => Request::METHOD_PATCH,
-            'method' => Request::METHOD_POST,
-        ]);
+        try {
+            $productForm = $this->getEditProductFormBuilder()->getFormFor($productId, [], [
+                'product_id' => $productId,
+                // @todo: patch/partial update doesn't work good for now (especially multiple empty values) so we use POST for now
+                // 'method' => Request::METHOD_PATCH,
+                'method' => Request::METHOD_POST,
+            ]);
+        } catch (ShopAssociationNotFound $e) {
+            return $this->renderMissingAssociation($productId);
+        }
 
         try {
             $productForm->handleRequest($request);
@@ -477,6 +482,26 @@ class ProductController extends FrameworkBundleAdminController
                 ]
             )
         );
+    }
+
+    /**
+     * @param int $productId
+     *
+     * @return Response
+     */
+    private function renderMissingAssociation(int $productId): Response
+    {
+        //@todo this error message should be improved to indicate which shop can/should be used for this product and/or how to associate it to the current context
+        return $this->render('@PrestaShop/Admin/Sell/Catalog/Product/disabled.html.twig', [
+            'errorMessage' => $this->trans(
+                'This product is not associated to the store selected in the current context, please select another one.',
+                'Admin.Notifications.Info',
+            ),
+            'standardPageUrl' => $this->generateUrl(
+                !empty($productId) ? 'admin_product_form' : 'admin_product_new',
+                !empty($productId) ? ['id' => $productId] : []
+            ),
+        ]);
     }
 
     /**