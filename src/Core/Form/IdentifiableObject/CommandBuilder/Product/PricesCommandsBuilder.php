PrestaShop/PrestaShop

Enable multistore support in prices form and in command builder

src/Core/Form/IdentifiableObject/CommandBuilder/Product/PricesCommandsBuilder.php
@@ -66,13 +66,13 @@ class PricesCommandsBuilder implements MultistoreProductCommandsBuilderInterface
         $priceData = $formData['pricing'];
         $config = new CommandAccessorConfig($this->modifyAllNamePrefix);
         $config
-            ->addField('[retail_price][price_tax_excluded]', 'setPrice', CommandField::TYPE_STRING)
-            ->addField('[retail_price][ecotax]', 'setEcotax', CommandField::TYPE_STRING)
-            ->addField('[tax_rules_group_id]', 'setTaxRulesGroupId', CommandField::TYPE_STRING)
-            ->addField('[on_sale]', 'setOnSale', CommandField::TYPE_STRING)
-            ->addField('[wholesale_price]', 'setWholesalePrice', CommandField::TYPE_STRING)
-            ->addField('[unit_price][price]', 'setUnitPrice', CommandField::TYPE_STRING)
-            ->addField('[unit_price][unity]', 'setUnity', CommandField::TYPE_STRING)
+            ->addMultiStoreField('[retail_price][price_tax_excluded]', 'setPrice', CommandField::TYPE_STRING)
+            ->addMultiStoreField('[retail_price][ecotax]', 'setEcotax', CommandField::TYPE_STRING)
+            ->addMultiStoreField('[tax_rules_group_id]', 'setTaxRulesGroupId', CommandField::TYPE_STRING)
+            ->addMultiStoreField('[on_sale]', 'setOnSale', CommandField::TYPE_STRING)
+            ->addMultiStoreField('[wholesale_price]', 'setWholesalePrice', CommandField::TYPE_STRING)
+            ->addMultiStoreField('[unit_price][price]', 'setUnitPrice', CommandField::TYPE_STRING)
+            ->addMultiStoreField('[unit_price][unity]', 'setUnity', CommandField::TYPE_STRING)
         ;
 
         $commandAccessor = new CommandAccessor($config);

leedzUp/leedzup

src/Core/Form/IdentifiableObject/CommandBuilder/Product/PricesCommandsBuilder.php
@@ -66,13 +66,13 @@ class PricesCommandsBuilder implements MultistoreProductCommandsBuilderInterface
         $priceData = $formData['pricing'];
         $config = new CommandAccessorConfig($this->modifyAllNamePrefix);
         $config
-            ->addField('[retail_price][price_tax_excluded]', 'setPrice', CommandField::TYPE_STRING)
-            ->addField('[retail_price][ecotax]', 'setEcotax', CommandField::TYPE_STRING)
-            ->addField('[tax_rules_group_id]', 'setTaxRulesGroupId', CommandField::TYPE_STRING)
-            ->addField('[on_sale]', 'setOnSale', CommandField::TYPE_STRING)
-            ->addField('[wholesale_price]', 'setWholesalePrice', CommandField::TYPE_STRING)
-            ->addField('[unit_price][price_tax_excluded]', 'setUnitPrice', CommandField::TYPE_STRING)
-            ->addField('[unit_price][unity]', 'setUnity', CommandField::TYPE_STRING)
+            ->addMultiStoreField('[retail_price][price_tax_excluded]', 'setPrice', CommandField::TYPE_STRING)
+            ->addMultiStoreField('[retail_price][ecotax]', 'setEcotax', CommandField::TYPE_STRING)
+            ->addMultiStoreField('[tax_rules_group_id]', 'setTaxRulesGroupId', CommandField::TYPE_STRING)
+            ->addMultiStoreField('[on_sale]', 'setOnSale', CommandField::TYPE_STRING)
+            ->addMultiStoreField('[wholesale_price]', 'setWholesalePrice', CommandField::TYPE_STRING)
+            ->addMultiStoreField('[unit_price][price_tax_excluded]', 'setUnitPrice', CommandField::TYPE_STRING)
+            ->addMultiStoreField('[unit_price][unity]', 'setUnity', CommandField::TYPE_STRING)
         ;
 
         $commandAccessor = new CommandAccessor($config);