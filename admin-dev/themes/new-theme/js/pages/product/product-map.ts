PrestaShop/PrestaShop

Finish feedbacks

admin-dev/themes/new-theme/js/pages/product/product-map.ts
@@ -91,16 +91,16 @@ export default {
     editCombinationButtons: '.edit-combination-item',
     tableRow: {
       combinationImg: '.combination-image',
-      combinationCheckbox: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_is_selected`,
-      combinationIdInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_combination_id`,
-      combinationNameInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_name`,
-      referenceInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_reference_value`,
-      impactOnPriceInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_impact_on_price_value`,
-      finalPriceTeInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_final_price_te`,
-      quantityInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_quantity_value`,
-      isDefaultInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_is_default`,
-      editButton: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_edit`,
-      deleteButton: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_delete`,
+      combinationCheckbox: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_is_selected`,
+      combinationIdInput: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_combination_id`,
+      combinationNameInput: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_name`,
+      referenceInput: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_reference_value`,
+      impactOnPriceInput: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_impact_on_price_value`,
+      finalPriceTeInput: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_final_price_te`,
+      quantityInput: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_quantity_value`,
+      isDefaultInput: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_is_default`,
+      editButton: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_edit`,
+      deleteButton: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_delete`,
     },
     editModal: '#combination-edit-modal',
     images: {

leedzUp/leedzup

admin-dev/themes/new-theme/js/pages/product/product-map.ts
@@ -102,17 +102,17 @@ export default {
     tableRow: {
       combinationImg: '.combination-image',
       deltaQuantityWrapper: '.delta-quantity',
-      deltaQuantityInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_delta_quantity_delta`,
-      combinationCheckbox: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_is_selected`,
-      combinationIdInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_combination_id`,
-      combinationNameInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_name`,
-      referenceInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_reference_value`,
-      impactOnPriceInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_impact_on_price_value`,
-      finalPriceTeInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_final_price_te`,
-      quantityInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_delta_quantity_quantity`,
-      isDefaultInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_is_default`,
-      editButton: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_edit`,
-      deleteButton: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_delete`,
+      deltaQuantityInput: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_delta_quantity_delta`,
+      combinationCheckbox: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_is_selected`,
+      combinationIdInput: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_combination_id`,
+      combinationNameInput: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_name`,
+      referenceInput: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_reference_value`,
+      impactOnPriceInput: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_impact_on_price_value`,
+      finalPriceTeInput: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_final_price_te`,
+      quantityInput: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_delta_quantity_quantity`,
+      isDefaultInput: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_is_default`,
+      editButton: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_edit`,
+      deleteButton: (rowIndex: number): string => `${combinationListId}_combinations_${rowIndex}_delete`,
     },
     editModal: '#combination-edit-modal',
     images: {