PrestaShop/PrestaShop

Migrate maps files

admin-dev/themes/new-theme/js/pages/product/product-map.ts
@@ -91,16 +91,16 @@ export default {
     editCombinationButtons: '.edit-combination-item',
     tableRow: {
       combinationImg: '.combination-image',
-      combinationCheckbox: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_is_selected`,
-      combinationIdInput: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_combination_id`,
-      combinationNameInput: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_name`,
-      referenceInput: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_reference_value`,
-      impactOnPriceInput: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_impact_on_price_value`,
-      finalPriceTeInput: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_final_price_te`,
-      quantityInput: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_quantity_value`,
-      isDefaultInput: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_is_default`,
-      editButton: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_edit`,
-      deleteButton: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_delete`,
+      combinationCheckbox: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_is_selected`,
+      combinationIdInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_combination_id`,
+      combinationNameInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_name`,
+      referenceInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_reference_value`,
+      impactOnPriceInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_impact_on_price_value`,
+      finalPriceTeInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_final_price_te`,
+      quantityInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_quantity_value`,
+      isDefaultInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_is_default`,
+      editButton: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_edit`,
+      deleteButton: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_delete`,
     },
     editModal: '#combination-edit-modal',
     images: {
@@ -131,8 +131,8 @@ export default {
     dropzoneContainer: '.dropzone-container',
     checkbox: '.md-checkbox input',
     shownTooltips: '.tooltip.show',
-    savedImageContainer: (imageId) => `.dz-preview[data-id="${imageId}"]`,
-    savedImage: (imageId) => `.dz-preview[data-id="${imageId}"] img`,
+    savedImageContainer: (imageId: string): string => `.dz-preview[data-id="${imageId}"]`,
+    savedImage: (imageId: string): string => `.dz-preview[data-id="${imageId}"] img`,
     coveredPreview: '.dz-preview.is-cover',
     windowFileManager: '.dropzone-window-filemanager',
   },
@@ -179,7 +179,7 @@ export default {
     treeCheckboxInput: '.tree-checkbox-input',
     checkboxInput: '[type=checkbox]',
     checkedCheckboxInputs: '[type=checkbox]:checked',
-    inputByValue: (value) => `input[value="${value}"]`,
+    inputByValue: (value: string): string => `input[value="${value}"]`,
     defaultCategorySelectInput: '#product_description_categories_default_category_id',
     materialCheckbox: '.md-checkbox',
     defaultRadioInput: '[type=radio]:checked',
@@ -202,10 +202,10 @@ export default {
     selectorContainer: '.module-selection',
     moduleSelector: '.modules-list-select',
     selectorPreviews: '.module-selection .module-render-container',
-    selectorPreview: (moduleId) => `.module-selection .module-render-container.${moduleId}`,
+    selectorPreview: (moduleId: string): string => `.module-selection .module-render-container.${moduleId}`,
     contentContainer: '.module-contents',
     moduleContents: '.module-contents .module-render-container',
-    moduleContent: (moduleId) => `.module-contents .module-render-container.${moduleId}`,
+    moduleContent: (moduleId: string): string => `.module-contents .module-render-container.${moduleId}`,
   },
   attachments: {
     attachmentsContainer: attachmentsBlockId,

leedzUp/leedzup

admin-dev/themes/new-theme/js/pages/product/product-map.ts
@@ -102,17 +102,17 @@ export default {
     tableRow: {
       combinationImg: '.combination-image',
       deltaQuantityWrapper: '.delta-quantity',
-      combinationCheckbox: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_is_selected`,
-      combinationIdInput: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_combination_id`,
-      combinationNameInput: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_name`,
-      referenceInput: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_reference_value`,
-      impactOnPriceInput: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_impact_on_price_value`,
-      finalPriceTeInput: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_final_price_te`,
-      quantityInput: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_delta_quantity_quantity`,
-      deltaQuantityInput: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_delta_quantity_delta`,
-      isDefaultInput: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_is_default`,
-      editButton: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_edit`,
-      deleteButton: (rowIndex) => `${combinationListId}_combinations_${rowIndex}_delete`,
+      deltaQuantityInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_delta_quantity_delta`,
+      combinationCheckbox: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_is_selected`,
+      combinationIdInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_combination_id`,
+      combinationNameInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_name`,
+      referenceInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_reference_value`,
+      impactOnPriceInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_impact_on_price_value`,
+      finalPriceTeInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_final_price_te`,
+      quantityInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_delta_quantity_quantity`,
+      isDefaultInput: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_is_default`,
+      editButton: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_edit`,
+      deleteButton: (rowIndex: string): string => `${combinationListId}_combinations_${rowIndex}_delete`,
     },
     editModal: '#combination-edit-modal',
     images: {
@@ -143,8 +143,8 @@ export default {
     dropzoneContainer: '.dropzone-container',
     checkbox: '.md-checkbox input',
     shownTooltips: '.tooltip.show',
-    savedImageContainer: (imageId) => `.dz-preview[data-id="${imageId}"]`,
-    savedImage: (imageId) => `.dz-preview[data-id="${imageId}"] img`,
+    savedImageContainer: (imageId: string): string => `.dz-preview[data-id="${imageId}"]`,
+    savedImage: (imageId: string): string => `.dz-preview[data-id="${imageId}"] img`,
     coveredPreview: '.dz-preview.is-cover',
     windowFileManager: '.dropzone-window-filemanager',
   },
@@ -191,7 +191,7 @@ export default {
     treeCheckboxInput: '.tree-checkbox-input',
     checkboxInput: '[type=checkbox]',
     checkedCheckboxInputs: '[type=checkbox]:checked',
-    inputByValue: (value) => `input[value="${value}"]`,
+    inputByValue: (value: string): string => `input[value="${value}"]`,
     defaultCategorySelectInput: '#product_description_categories_default_category_id',
     materialCheckbox: '.md-checkbox',
     defaultRadioInput: '[type=radio]:checked',
@@ -214,10 +214,10 @@ export default {
     selectorContainer: '.module-selection',
     moduleSelector: '.modules-list-select',
     selectorPreviews: '.module-selection .module-render-container',
-    selectorPreview: (moduleId) => `.module-selection .module-render-container.${moduleId}`,
+    selectorPreview: (moduleId: string): string => `.module-selection .module-render-container.${moduleId}`,
     contentContainer: '.module-contents',
     moduleContents: '.module-contents .module-render-container',
-    moduleContent: (moduleId) => `.module-contents .module-render-container.${moduleId}`,
+    moduleContent: (moduleId: string): string => `.module-contents .module-render-container.${moduleId}`,
   },
   attachments: {
     attachmentsContainer: attachmentsBlockId,