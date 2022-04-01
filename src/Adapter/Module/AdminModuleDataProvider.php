PrestaShop/PrestaShop

Apply suggestions from code review
Co-authored-by: Ibrahima SOW <sowbiba@hotmail.com>

src/Adapter/Module/AdminModuleDataProvider.php
@@ -214,7 +214,7 @@ class AdminModuleDataProvider implements ModuleInterface
 
             if ($module->isInstalled()) {
                 unset($urls['install']);
-                if (!$moduleDatabaseAttributes->getBoolean('active')) {
+                if (!$module->isActive()) {
                     unset(
                         $urls['disable'],
                         $urls['enable_mobile'],
@@ -226,7 +226,7 @@ class AdminModuleDataProvider implements ModuleInterface
                 } else {
                     unset(
                         $urls['enable'],
-                        $urls[$moduleDatabaseAttributes->getBoolean('active_on_mobile') ? 'enable_mobile' : 'disable_mobile']
+                        $urls[$module->isActiveOnMobile() ? 'enable_mobile' : 'disable_mobile']
                     );
                 }
 
@@ -234,7 +234,7 @@ class AdminModuleDataProvider implements ModuleInterface
                     unset($urls['upgrade']);
                 }
 
-                if (!$moduleAttributes->getBoolean('is_configurable')) {
+                if (!$module->isConfigurable()) {
                     unset($urls['configure']);
                 }
             } else {

leedzUp/leedzup

src/Adapter/Module/AdminModuleDataProvider.php
@@ -229,7 +229,7 @@ class AdminModuleDataProvider implements ModuleInterface
 
             if ($module->isInstalled()) {
                 unset($urls['install']);
-                if (!$moduleDatabaseAttributes->getBoolean('active')) {
+                if (!$module->isActive()) {
                     unset(
                         $urls['disable'],
                         $urls['enableMobile'],
@@ -241,7 +241,7 @@ class AdminModuleDataProvider implements ModuleInterface
                 } else {
                     unset(
                         $urls['enable'],
-                        $urls[$moduleDatabaseAttributes->getBoolean('active_on_mobile') ? 'enableMobile' : 'disableMobile']
+                        $urls[$module->isActiveOnMobile() ? 'enableMobile' : 'disableMobile']
                     );
                 }
 
@@ -249,7 +249,7 @@ class AdminModuleDataProvider implements ModuleInterface
                     unset($urls['upgrade']);
                 }
 
-                if (!$moduleAttributes->getBoolean('is_configurable')) {
+                if (!$module->isConfigurable()) {
                     unset($urls['configure']);
                 }
             } else {