PrestaShop/PrestaShop

Fixed the wording translation for Enable/Disable Mobile in Modules page

src/PrestaShopBundle/Command/ModuleCommand.php
@@ -28,6 +28,7 @@ namespace PrestaShopBundle\Command;
 
 use Employee;
 use PrestaShop\PrestaShop\Adapter\LegacyContext;
+use PrestaShop\PrestaShop\Adapter\Module\AdminModuleDataProvider;
 use PrestaShop\PrestaShop\Adapter\Module\Configuration\ModuleSelfConfigurator;
 use PrestaShop\PrestaShop\Core\Addon\Module\ModuleManager;
 use Symfony\Component\Console\Command\Command;
@@ -190,7 +191,7 @@ class ModuleCommand extends Command
                 $this->translator->trans(
                     '%action% action on module %module% succeeded.',
                     [
-                        '%action%' => ucfirst(str_replace('_', ' ', $action)),
+                        '%action%' => ucfirst(AdminModuleDataProvider::_ACTIONS_TRANSLATION_LABELS_[$action]),
                         '%module%' => $moduleName, ],
                     'Admin.Modules.Notification'
                 )

src/PrestaShopBundle/Controller/Admin/Improve/ModuleController.php
@@ -224,7 +224,7 @@ class ModuleController extends ModuleAbstractController
             return new JsonResponse($response);
         }
 
-        $actionTitle = str_replace('_', ' ', $action);
+        $actionTitle = AdminModuleDataProvider::_ACTIONS_TRANSLATION_LABELS_[$action];
 
         try {
             $response[$module]['status'] = $moduleManager->{$action}($module);

leedzUp/leedzup

src/PrestaShopBundle/Command/ModuleCommand.php
@@ -28,6 +28,7 @@ namespace PrestaShopBundle\Command;
 
 use Employee;
 use PrestaShop\PrestaShop\Adapter\LegacyContext;
+use PrestaShop\PrestaShop\Adapter\Module\AdminModuleDataProvider;
 use PrestaShop\PrestaShop\Adapter\Module\Configuration\ModuleSelfConfigurator;
 use PrestaShop\PrestaShop\Core\Addon\Module\ModuleManager;
 use Symfony\Component\Console\Command\Command;
@@ -190,7 +191,7 @@ class ModuleCommand extends Command
                 $this->translator->trans(
                     '%action% action on module %module% succeeded.',
                     [
-                        '%action%' => ucfirst(str_replace('_', ' ', $action)),
+                        '%action%' => ucfirst(AdminModuleDataProvider::ACTIONS_TRANSLATION_LABELS[$action]),
                         '%module%' => $moduleName, ],
                     'Admin.Modules.Notification'
                 )

src/PrestaShopBundle/Controller/Admin/Improve/ModuleController.php
@@ -224,7 +224,7 @@ class ModuleController extends ModuleAbstractController
             return new JsonResponse($response);
         }
 
-        $actionTitle = str_replace('_', ' ', $action);
+        $actionTitle = AdminModuleDataProvider::ACTIONS_TRANSLATION_LABELS[$action];
 
         try {
             $response[$module]['status'] = $moduleManager->{$action}($module);