PrestaShop/PrestaShop

Migrate the Customers > Title pages (Only the list)

src/Core/Form/ChoiceProvider/GenderChoiceProvider.php
@@ -1,0 +1,63 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\PrestaShop\Core\Form\ChoiceProvider;
+
+use PrestaShop\PrestaShop\Adapter\Entity\Gender;
+use PrestaShop\PrestaShop\Core\Form\FormChoiceProviderInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * Class GenderProvider provides genders choices.
+ */
+class GenderChoiceProvider implements FormChoiceProviderInterface
+{
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
+     * Get currency choices.
+     *
+     * @return array
+     */
+    public function getChoices(): array
+    {
+        return [
+            $this->translator->trans('Male', [], 'Admin.Shopparameters.Feature') => Gender::GENDER_MALE,
+            $this->translator->trans('Female', [], 'Admin.Shopparameters.Feature') => Gender::GENDER_FEMALE,
+            $this->translator->trans('Neutral', [], 'Admin.Shopparameters.Feature') => Gender::GENDER_NEUTRAL,
+        ];
+    }
+}

src/Core/Grid/Data/Factory/TitleGridDataFactory.php
@@ -1,0 +1,116 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\PrestaShop\Core\Grid\Data\Factory;
+
+use PrestaShop\PrestaShop\Adapter\Entity\Gender;
+use PrestaShop\PrestaShop\Core\Grid\Data\GridData;
+use PrestaShop\PrestaShop\Core\Grid\Record\RecordCollection;
+use PrestaShop\PrestaShop\Core\Grid\Search\SearchCriteriaInterface;
+use PrestaShop\PrestaShop\Core\Image\ImageProviderInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * Class TitleGridDataFactory gets data for title grid.
+ */
+class TitleGridDataFactory implements GridDataFactoryInterface
+{
+    /**
+     * @var GridDataFactoryInterface
+     */
+    private $doctrineTitleDataFactory;
+
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    /**
+     * @var ImageProviderInterface
+     */
+    private $titleImageThumbnailProvider;
+
+    /**
+     * @param GridDataFactoryInterface $doctrineTitleDataFactory
+     * @param TranslatorInterface $translator
+     * @param ImageProviderInterface $titleImageThumbnailProvider
+     */
+    public function __construct(
+        GridDataFactoryInterface $doctrineTitleDataFactory,
+        TranslatorInterface $translator,
+        ImageProviderInterface $titleImageThumbnailProvider
+    ) {
+        $this->doctrineTitleDataFactory = $doctrineTitleDataFactory;
+        $this->translator = $translator;
+        $this->titleImageThumbnailProvider = $titleImageThumbnailProvider;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getData(SearchCriteriaInterface $searchCriteria)
+    {
+        $titleData = $this->doctrineTitleDataFactory->getData($searchCriteria);
+
+        $modifiedRecords = $this->applyModification(
+            $titleData->getRecords()->all()
+        );
+
+        return new GridData(
+            new RecordCollection($modifiedRecords),
+            $titleData->getRecordsTotal(),
+            $titleData->getQuery()
+        );
+    }
+
+    /**
+     * @param array $titles
+     *
+     * @return array
+     */
+    private function applyModification(array $titles): array
+    {
+        foreach ($titles as $i => $title) {
+            switch ($title['type']) {
+                case Gender::GENDER_MALE:
+                    $titles[$i]['type'] = $this->translator->trans('Male', [], 'Admin.Shopparameters.Feature');
+                    break;
+                case Gender::GENDER_FEMALE:
+                    $titles[$i]['type'] = $this->translator->trans('Female', [], 'Admin.Shopparameters.Feature');
+                    break;
+                case Gender::GENDER_NEUTRAL:
+                    $titles[$i]['type'] = $this->translator->trans('Neutral', [], 'Admin.Shopparameters.Feature');
+                    break;
+            }
+
+            $titles[$i]['image'] = $this->titleImageThumbnailProvider->getPath($title['id_gender']);
+        }
+
+        return $titles;
+    }
+}

src/Core/Grid/Query/TitleQueryBuilder.php
@@ -1,0 +1,138 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\PrestaShop\Core\Grid\Query;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Query\QueryBuilder;
+use PrestaShop\PrestaShop\Core\Grid\Search\SearchCriteriaInterface;
+
+/**
+ * Class TitleQueryBuilder provides query builders for titles grid.
+ */
+class TitleQueryBuilder extends AbstractDoctrineQueryBuilder
+{
+    /**
+     * @var DoctrineSearchCriteriaApplicatorInterface
+     */
+    private $searchCriteriaApplicator;
+
+    /**
+     * @var int
+     */
+    private $languageId;
+
+    /**
+     * @param Connection $connection
+     * @param string $dbPrefix
+     * @param DoctrineSearchCriteriaApplicatorInterface $searchCriteriaApplicator
+     * @param int $languageId
+     */
+    public function __construct(
+        Connection $connection,
+        string $dbPrefix,
+        DoctrineSearchCriteriaApplicatorInterface $searchCriteriaApplicator,
+        int $languageId
+    ) {
+        parent::__construct($connection, $dbPrefix);
+
+        $this->searchCriteriaApplicator = $searchCriteriaApplicator;
+        $this->languageId = $languageId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSearchQueryBuilder(SearchCriteriaInterface $searchCriteria)
+    {
+        $builder = $this->getTitleQueryBuilder($searchCriteria)
+            ->select('t.*, tl.name');
+
+        $this->searchCriteriaApplicator
+            ->applySorting($searchCriteria, $builder)
+            ->applyPagination($searchCriteria, $builder);
+
+        return $builder;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCountQueryBuilder(SearchCriteriaInterface $searchCriteria)
+    {
+        return $this->getTitleQueryBuilder($searchCriteria)->select('COUNT(t.id_gender)');
+    }
+
+    /**
+     * @param SearchCriteriaInterface $searchCriteria
+     *
+     * @return QueryBuilder
+     */
+    private function getTitleQueryBuilder(SearchCriteriaInterface $searchCriteria): QueryBuilder
+    {
+        $builder = $this->connection->createQueryBuilder()
+            ->from($this->dbPrefix . 'gender', 't')
+            ->innerJoin('t', $this->dbPrefix . 'gender_lang', 'tl', 't.id_gender = tl.id_gender')
+            ->andWhere('tl.`id_lang`= :language')
+            ->setParameter('language', $this->languageId)
+        ;
+
+        $this->applyFilters($builder, $searchCriteria);
+
+        return $builder;
+    }
+
+    /**
+     * @param QueryBuilder $builder
+     * @param SearchCriteriaInterface $searchCriteria
+     */
+    private function applyFilters(QueryBuilder $builder, SearchCriteriaInterface $searchCriteria): void
+    {
+        $allowedFilters = [
+            'id_gender',
+            'type',
+            'name',
+        ];
+
+        foreach ($searchCriteria->getFilters() as $filterName => $filterValue) {
+            if (!in_array($filterName, $allowedFilters)) {
+                continue;
+            }
+
+            if ($filterName === 'id_gender') {
+                $builder->andWhere('t.' . $filterName . ' = :' . $filterName);
+                $builder->setParameter($filterName, $filterValue);
+
+                continue;
+            }
+
+            $builder->andWhere($filterName . ' LIKE :' . $filterName);
+            $builder->setParameter($filterName, '%' . $filterValue . '%');
+        }
+    }
+}

leedzUp/leedzup

src/Core/Form/ChoiceProvider/GenderChoiceProvider.php
@@ -1,0 +1,63 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\PrestaShop\Core\Form\ChoiceProvider;
+
+use PrestaShop\PrestaShop\Adapter\Entity\Gender;
+use PrestaShop\PrestaShop\Core\Form\FormChoiceProviderInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * Class GenderProvider provides genders choices.
+ */
+class GenderChoiceProvider implements FormChoiceProviderInterface
+{
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
+     * Get currency choices.
+     *
+     * @return array
+     */
+    public function getChoices(): array
+    {
+        return [
+            $this->translator->trans('Male', [], 'Admin.Shopparameters.Feature') => Gender::TYPE_MALE,
+            $this->translator->trans('Female', [], 'Admin.Shopparameters.Feature') => Gender::TYPE_FEMALE,
+            $this->translator->trans('Other', [], 'Admin.Shopparameters.Feature') => Gender::TYPE_OTHER,
+        ];
+    }
+}

src/Core/Grid/Data/Factory/TitleGridDataFactory.php
@@ -1,0 +1,116 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\PrestaShop\Core\Grid\Data\Factory;
+
+use PrestaShop\PrestaShop\Adapter\Entity\Gender;
+use PrestaShop\PrestaShop\Core\Grid\Data\GridData;
+use PrestaShop\PrestaShop\Core\Grid\Record\RecordCollection;
+use PrestaShop\PrestaShop\Core\Grid\Search\SearchCriteriaInterface;
+use PrestaShop\PrestaShop\Core\Image\ImageProviderInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * Class TitleGridDataFactory gets data for title grid.
+ */
+class TitleGridDataFactory implements GridDataFactoryInterface
+{
+    /**
+     * @var GridDataFactoryInterface
+     */
+    private $doctrineTitleDataFactory;
+
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    /**
+     * @var ImageProviderInterface
+     */
+    private $titleImageThumbnailProvider;
+
+    /**
+     * @param GridDataFactoryInterface $doctrineTitleDataFactory
+     * @param TranslatorInterface $translator
+     * @param ImageProviderInterface $titleImageThumbnailProvider
+     */
+    public function __construct(
+        GridDataFactoryInterface $doctrineTitleDataFactory,
+        TranslatorInterface $translator,
+        ImageProviderInterface $titleImageThumbnailProvider
+    ) {
+        $this->doctrineTitleDataFactory = $doctrineTitleDataFactory;
+        $this->translator = $translator;
+        $this->titleImageThumbnailProvider = $titleImageThumbnailProvider;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getData(SearchCriteriaInterface $searchCriteria)
+    {
+        $titleData = $this->doctrineTitleDataFactory->getData($searchCriteria);
+
+        $modifiedRecords = $this->applyModification(
+            $titleData->getRecords()->all()
+        );
+
+        return new GridData(
+            new RecordCollection($modifiedRecords),
+            $titleData->getRecordsTotal(),
+            $titleData->getQuery()
+        );
+    }
+
+    /**
+     * @param array $titles
+     *
+     * @return array
+     */
+    private function applyModification(array $titles): array
+    {
+        foreach ($titles as $i => $title) {
+            switch ($title['type']) {
+                case Gender::TYPE_MALE:
+                    $titles[$i]['type'] = $this->translator->trans('Male', [], 'Admin.Shopparameters.Feature');
+                    break;
+                case Gender::TYPE_FEMALE:
+                    $titles[$i]['type'] = $this->translator->trans('Female', [], 'Admin.Shopparameters.Feature');
+                    break;
+                default:
+                    $titles[$i]['type'] = $this->translator->trans('Other', [], 'Admin.Shopparameters.Feature');
+                    break;
+            }
+
+            $titles[$i]['image'] = $this->titleImageThumbnailProvider->getPath($title['id_gender']);
+        }
+
+        return $titles;
+    }
+}

src/Core/Grid/Query/TitleQueryBuilder.php
@@ -1,0 +1,138 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\PrestaShop\Core\Grid\Query;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Query\QueryBuilder;
+use PrestaShop\PrestaShop\Core\Grid\Search\SearchCriteriaInterface;
+
+/**
+ * Class TitleQueryBuilder provides query builders for titles grid.
+ */
+class TitleQueryBuilder extends AbstractDoctrineQueryBuilder
+{
+    /**
+     * @var DoctrineSearchCriteriaApplicatorInterface
+     */
+    private $searchCriteriaApplicator;
+
+    /**
+     * @var int
+     */
+    private $languageId;
+
+    /**
+     * @param Connection $connection
+     * @param string $dbPrefix
+     * @param DoctrineSearchCriteriaApplicatorInterface $searchCriteriaApplicator
+     * @param int $languageId
+     */
+    public function __construct(
+        Connection $connection,
+        string $dbPrefix,
+        DoctrineSearchCriteriaApplicatorInterface $searchCriteriaApplicator,
+        int $languageId
+    ) {
+        parent::__construct($connection, $dbPrefix);
+
+        $this->searchCriteriaApplicator = $searchCriteriaApplicator;
+        $this->languageId = $languageId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSearchQueryBuilder(SearchCriteriaInterface $searchCriteria)
+    {
+        $builder = $this->getTitleQueryBuilder($searchCriteria)
+            ->select('g.*, gl.name');
+
+        $this->searchCriteriaApplicator
+            ->applySorting($searchCriteria, $builder)
+            ->applyPagination($searchCriteria, $builder);
+
+        return $builder;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCountQueryBuilder(SearchCriteriaInterface $searchCriteria)
+    {
+        return $this->getTitleQueryBuilder($searchCriteria)->select('COUNT(g.id_gender)');
+    }
+
+    /**
+     * @param SearchCriteriaInterface $searchCriteria
+     *
+     * @return QueryBuilder
+     */
+    private function getTitleQueryBuilder(SearchCriteriaInterface $searchCriteria): QueryBuilder
+    {
+        $builder = $this->connection->createQueryBuilder()
+            ->from($this->dbPrefix . 'gender', 'g')
+            ->innerJoin('g', $this->dbPrefix . 'gender_lang', 'gl', 'g.id_gender = gl.id_gender')
+            ->andWhere('gl.`id_lang`= :language')
+            ->setParameter('language', $this->languageId)
+        ;
+
+        $this->applyFilters($builder, $searchCriteria);
+
+        return $builder;
+    }
+
+    /**
+     * @param QueryBuilder $builder
+     * @param SearchCriteriaInterface $searchCriteria
+     */
+    private function applyFilters(QueryBuilder $builder, SearchCriteriaInterface $searchCriteria): void
+    {
+        $allowedFilters = [
+            'id_gender',
+            'type',
+            'name',
+        ];
+
+        foreach ($searchCriteria->getFilters() as $filterName => $filterValue) {
+            if (!in_array($filterName, $allowedFilters)) {
+                continue;
+            }
+
+            if ($filterName === 'type') {
+                $builder->andWhere('g.' . $filterName . ' = :' . $filterName);
+                $builder->setParameter($filterName, $filterValue);
+
+                continue;
+            }
+
+            $builder->andWhere($filterName . ' LIKE :' . $filterName);
+            $builder->setParameter($filterName, '%' . $filterValue . '%');
+        }
+    }
+}