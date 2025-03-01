c11afc77f92d0ddbb40f4a54b250afe1f1979662
Greg <social@leedzup.com>
2025-03-01 08:38:01 +0100

    correct sql

--- install-dev/data/db_data.sql
+++ install-dev/data/db_data.sql
@@ -1,9 +1,9 @@
 SET
-  SET SESSION sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+  SESSION sql_mode='';
 SET
   NAMES 'utf8mb4';
 
 /* Here you can insert data for installation with SQL requests */
 
-INSERT INTO ps_image_type (name, width, height, products, categories, manufacturers, suppliers, stores)
+INSERT INTO `PREFIX_image_type` (name, width, height, products, categories, manufacturers, suppliers, stores)
 VALUES ('home_default', 250, 250, 1, 0, 0, 0, 0);
