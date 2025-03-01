c749b65207e357984e8fcf5d8d5f0b8146344560
Greg <social@leedzup.com>
2025-03-01 07:08:43 +0100

    change default install theme

--- install-dev/controllers/console/process.php
+++ install-dev/controllers/console/process.php
@@ -328,7 +328,15 @@ class InstallControllerConsoleProcess extends InstallControllerConsole implement
     {
         $this->initializeContext();
 
-        return $this->model_install->installTheme($this->datas->theme);
+        // Définis ici le nom exact de ton thème
+        $themeName = 'almodovar';
+
+        // On force l'installation de ton thème
+        $this->datas->theme = $themeName;
+        Configuration::updateValue('PS_THEME', $themeName);
+        Configuration::updateValue('PS_THEME_DEFAULT', $themeName);
+
+        return $this->model_install->installTheme($themeName);
     }
 
     private function clearConfigXML()
