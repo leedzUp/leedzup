32c33673d18b4b53c1bb23a1fbd76764a36df84a
Greg <social@leedzup.com>
2025-03-02 14:16:45 +0100

    add rebuild on nstall

--- modules/ps_facetedsearch/rebuild_install.php
+++ modules/ps_facetedsearch/rebuild_install.php
@@ -7,14 +7,14 @@ require_once dirname(__FILE__).'/ps_facetedsearch.php';
 $module = Module::getInstanceByName('ps_facetedsearch');
 
 if ($module && method_exists($module, 'indexSurfaces')) {
-    $module->indexSurfaces(0,true);
+    $module->indexProductSurfaces();
     echo "Indexation de la surface reconstruite avec succès.\n";
 } else {
     echo "Erreur : La méthode indexSurfaces() n'existe pas.\n";
 }
 
 if ($module && method_exists($module, 'indexRooms')) {
-    $module->indexRooms(0,true);
+    $module->indexProductRooms();
     echo "Indexation de la room reconstruite avec succès.\n";
 } else {
     echo "Erreur : La méthode indexRooms() n'existe pas.\n";
