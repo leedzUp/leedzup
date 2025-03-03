8234ed7b3e905638b161fea5719f07c62c36abad
Greg <social@leedzup.com>
2025-03-03 07:41:49 +0100

    add index rebuild search

---
+++ modules/ps_facetedsearch/rebuild_install_search_index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once dirname(__FILE__).'/../../config/config.inc.php';
+require_once dirname(__FILE__).'/../../init.php';
+
+
+
+
+
+  // Considering the indexing task can be really long, we ask the PHP process to not stop before 2 hours.
+  ini_set('max_execution_time', '7200');
+  Search::indexation(true);
+
+
+
