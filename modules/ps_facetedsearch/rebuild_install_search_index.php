be1bb29cac8c713d89ec3d6793a42b82f2998875
Greg <social@leedzup.com>
2025-03-16 20:11:59 +0100

    ajax to reindex surface

--- modules/ps_facetedsearch/rebuild_install_search_index.php
+++ modules/ps_facetedsearch/rebuild_install_search_index.php
@@ -3,13 +3,48 @@
 require_once dirname(__FILE__).'/../../config/config.inc.php';
 require_once dirname(__FILE__).'/../../init.php';
 
+// Considering the indexing task can be really long, we ask the PHP process to not stop before 2 hours.
+ini_set('max_execution_time', '7200');
 
+$action = Tools::getValue('action');
 
+// Vérifie si le module existe avant de l'instancier
+if (!class_exists('Ps_Facetedsearch')) {
+    header('HTTP/1.1 500 Internal Server Error');
+    header('Content-Type: application/json');
+    die(json_encode(['error' => 'Module Ps_Facetedsearch not found']));
+}
 
+$module = new Ps_Facetedsearch();
+Shop::setContext(Shop::CONTEXT_ALL);
 
-  // Considering the indexing task can be really long, we ask the PHP process to not stop before 2 hours.
-  ini_set('max_execution_time', '7200');
-  Search::indexation(true);
+switch ($action) {
+    case 'indexSurfaces':
+        $cursor = (int) Tools::getValue('cursor');
+        $ajax = (bool) Tools::getValue('ajax');
+        $full = (bool) Tools::getValue('full');
 
+        $response = $full ? 
+            $module->fullSurfacesIndexProcess($cursor, $ajax, true) : 
+            $module->surfacesIndexProcess($cursor, $ajax);
+        
+        header('Content-Type: application/json');
+        die(json_encode($response));
 
+    case 'indexRooms':
+        $cursor = (int) Tools::getValue('cursor');
+        $ajax = (bool) Tools::getValue('ajax');
+        $full = (bool) Tools::getValue('full');
 
+        $response = $full ? 
+            $module->fullRoomsIndexProcess($cursor, $ajax, true) : 
+            $module->roomsIndexProcess($cursor, $ajax);
+        
+        header('Content-Type: application/json');
+        die(json_encode($response));
+
+    default:
+        header('HTTP/1.1 403 Forbidden');
+        header('Content-Type: application/json');
+        die(json_encode(['error' => 'Unknown action']));
+}
